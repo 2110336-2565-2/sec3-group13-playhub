e514a8b31b85685a96bca9dc7fda179e788736ce
Chinnapat.N <kharkkr@gmail.com>
2023-01-27 23:08:14 +0700

    Create Theme

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,21 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+
+const mainTheme = createTheme({
+  palette: {
+    primary: {
+      main: "#ffa31a",
+      light: "#ffe1b4",
+      dark: "#a66300",
+    },
+  },
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider theme={mainTheme}>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
 }
