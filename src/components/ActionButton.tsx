54c04970853dfe164623206cec999fb3cf3b5ffa
Chinnapat.N <kharkkr@gmail.com>
2023-01-27 23:56:26 +0700

    Add width constraint

--- src/components/ActionButton.tsx
+++ src/components/ActionButton.tsx
@@ -3,7 +3,9 @@ import Button from "@mui/material/Button";
 export default function ActionButton() {
   return (
     <>
-      <Button variant="contained">Login</Button>
+      <Button sx={{ width: "10vw", minWidth: "200px" }} variant="contained">
+        Login
+      </Button>
     </>
   );
 }
