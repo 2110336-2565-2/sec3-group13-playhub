12f065faa40e6ca823fb3a9b85913a4284c7eae5
Chinnapat.N <kharkkr@gmail.com>
2023-01-27 23:56:48 +0700

    Adjust component position

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -2,6 +2,7 @@ import * as React from "react";
 import Image from "next/image";
 import Grid from "@mui/material/Grid";
 import Link from "@mui/material/Link";
+import Box from "@mui/material/Box";
 
 import PasswordTextFeild from "@/components/PasswordTextFeild";
 import EmailTextFeild from "@/components/EmailTextFeild";
@@ -15,22 +16,27 @@ export default function Home() {
         direction="column"
         spacing={2}
         alignItems="center"
-        justifyItems="center"
+        justifyContent="center"
+        style={{ minHeight: "80vh" }}
       >
-        <Grid item xs={12}>
+        <Grid item>
           <Image src="/images/logo.png" height={119} width={119} alt="Logo" />
         </Grid>
-        <Grid item xs={12}>
+        <Grid item>
           <EmailTextFeild />
         </Grid>
-        <Grid item xs={12}>
+        <Grid item>
           <PasswordTextFeild />
         </Grid>
-        <Grid item xs={12}>
+        <Grid item>
           <ActionButton />
         </Grid>
-        <Grid item xs={2}>
-          <Link href="/register">Register?</Link>
+        <Grid item>
+          <Box sx={{ width: "20vw", minWidth: "260px" }}>
+            <Link color="neutral" underline="hover" href="/register">
+              Register?
+            </Link>
+          </Box>
         </Grid>
       </Grid>
     </>
