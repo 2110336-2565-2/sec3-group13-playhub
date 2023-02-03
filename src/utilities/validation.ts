b0cc785bf3787c8671b2dd416e957114592db54c
Chinnapat Nitipornsri <kharkkr@gmail.com>
2023-02-04 00:01:24 +0700

    Remove boolean type

--- src/utilities/validation.ts
+++ src/utilities/validation.ts
@@ -1,7 +1,7 @@
 const expression: RegExp =
   /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
-export function validateEmail(email: string): string | boolean {
+export function validateEmail(email: string): string {
   if (email.length === 0) {
     return "ช่องนี้ไม่สามารถเว้นว่างได้";
   }
