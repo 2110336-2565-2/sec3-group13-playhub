88c082add9633654fcbdc4a4aa509639ecaf587b
Chinnapat Nitipornsri <kharkkr@gmail.com>
2023-02-04 00:02:03 +0700

    Handle supabase response

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,23 +1,25 @@
 import React from "react";
-import Grid from "@mui/material/Grid";
-import Link from "@mui/material/Link";
-import Box from "@mui/material/Box";
-import { Typography, TextField } from "@mui/material";
+import { Link, Box, Typography, TextField, Stack } from "@mui/material";
 
 import PasswordTextFeild from "@/components/login/PasswordTextField";
 import CommonButton from "@/components/public/CommonButton";
-import { validateEmail, validateTextField } from "@/utilities/validation";
 import Logo from "@/components/public/Logo";
-import Stack from "@mui/material/Stack";
+import { validateEmail, validateTextField } from "@/utilities/validation";
 
 import { supabase } from "supabase/init";
-import { Session } from '@supabase/supabase-js'
-import { SUPABASE_LOGIN_CREDENTIALS_ERROR, SUPABASE_LOGIN_EMAIL_NOT_VALIDATED_ERROR } from "@/constants/authentication";
+import { Session } from "@supabase/supabase-js";
+import {
+  SUPABASE_LOGIN_CREDENTIALS_ERROR,
+  SUPABASE_LOGIN_EMAIL_NOT_VALIDATED_ERROR,
+} from "@/constants/authentication";
 
 // style
-const login_components = {
+const login_layout = {
   width: "23vw",
   minWidth: "260px",
+};
+
+const text_field = {
   height: "7vh",
   minHeight: "40px",
 };
@@ -26,7 +28,20 @@ export default function Home() {
   const [email, setEmail] = React.useState("");
   const [password, setPassword] = React.useState("");
   const [isSubmit, setIsSubmit] = React.useState(false);
-  const [foundUser, setFoundUser] = React.useState(true);
+  const [isLoginCredErr, setIsLoginCredErr] = React.useState(false);
+  const [isValidateErr, setIsValidateErr] = React.useState(false);
+
+  const emailErrMsg = validateEmail(email);
+  const isEmailErr = emailErrMsg !== "";
+
+  const passwordErrMsg = validateTextField(password, 1);
+  const isPasswordErr = passwordErrMsg !== "";
+
+  const isSupabaseErr =
+    (isLoginCredErr || isValidateErr) && !(isEmailErr || isPasswordErr);
+  const supabaseErrMsg = isLoginCredErr
+    ? "อีเมลหรือรหัสผ่านไม่ถูกต้อง"
+    : "โปรดทำการยืนยันอีเมล";
 
   // supabase use a little time to get current session, so some stall display might be needed
   const [isLoadingSession, setIsLoadingSession] = React.useState(true);
@@ -35,87 +50,105 @@ export default function Home() {
 
   async function handleSubmit() {
     setIsSubmit(true);
-    const signInResult = await supabase.auth.signInWithPassword({email, password});
-    if (signInResult.error && signInResult.error.message == SUPABASE_LOGIN_CREDENTIALS_ERROR) {
+    const signInResult = await supabase.auth.signInWithPassword({
+      email,
+      password,
+    });
+    if (
+      signInResult.error &&
+      signInResult.error.message == SUPABASE_LOGIN_CREDENTIALS_ERROR
+    ) {
       // wrong email or password
-      console.log("wrong email or password")
+      setIsLoginCredErr(true);
+      console.log("wrong email or password");
       return;
     }
-    if (signInResult.error && signInResult.error.message == SUPABASE_LOGIN_EMAIL_NOT_VALIDATED_ERROR) {
+    if (
+      signInResult.error &&
+      signInResult.error.message == SUPABASE_LOGIN_EMAIL_NOT_VALIDATED_ERROR
+    ) {
       // user have not validate email yet
-      console.log("You have not validate your email yet")
+      setIsValidateErr(true);
+      console.log("You have not validate your email yet");
       return;
     }
     setSession(signInResult.data.session);
   }
 
-  function handleEmailChange(event: React.ChangeEvent<HTMLTextAreaElement | HTMLInputElement>) {
+  function handleEmailChange(
+    event: React.ChangeEvent<HTMLTextAreaElement | HTMLInputElement>
+  ): void {
     setEmail(event.target.value);
     setIsSubmit(false);
-
-    setFoundUser(true);
+    // reset error
+    setIsLoginCredErr(false);
+    setIsValidateErr(false);
   }
 
-  function handlePasswordChange(event: React.ChangeEvent<HTMLTextAreaElement | HTMLInputElement>) {
+  function handlePasswordChange(
+    event: React.ChangeEvent<HTMLTextAreaElement | HTMLInputElement>
+  ): void {
     setPassword(event.target.value);
     setIsSubmit(false);
-
-    setFoundUser(true);
+    // reset error
+    setIsLoginCredErr(false);
+    setIsValidateErr(false);
   }
 
   async function handleSignOut() {
-    const signOutResult = await supabase.auth.signOut()
+    const signOutResult = await supabase.auth.signOut();
     if (signOutResult.error) {
-      console.log(signOutResult.error)
-      return
+      console.log(signOutResult.error);
+      return;
     }
-    setSession(null)
+    setSession(null);
   }
 
   React.useEffect(() => {
-    supabase.auth.getSession().then((getSessionResult) => {
-      setSession(getSessionResult.data.session)
-      setIsLoadingSession(false)
-    })
-  }, [])
+    supabase.auth.getSession().then((getSessionResult: any) => {
+      setSession(getSessionResult.data.session);
+      setIsLoadingSession(false);
+    });
+  }, []);
 
-  if (isLoadingSession) return <p>getting session...</p> // temporary display that supabase is processing
-  if (session != null) return <button onClick={handleSignOut}>logout</button> // temporay display if logged in
+  if (isLoadingSession) return <p>getting session...</p>; // temporary display that supabase is processing
+  if (session != null) return <button onClick={handleSignOut}>logout</button>; // temporay display if logged in
+
   return (
     <>
       <Stack
-        direction="column"
         spacing={3}
         alignItems="center"
         justifyContent="center"
         style={{ minHeight: "100vh" }}
       >
         <Logo width={119} height={119} />
+
         <TextField
-          sx={login_components}
+          sx={{ ...login_layout, ...text_field }}
           label="Email"
           onChange={handleEmailChange}
           value={email}
-          error={isSubmit && (validateEmail(email) !== "" || !foundUser)}
-          helperText={isSubmit && validateEmail(email)}
+          error={isSubmit && (isEmailErr || isSupabaseErr)}
+          helperText={isSubmit && emailErrMsg}
         />
 
         <PasswordTextFeild
           handleChange={handlePasswordChange}
           value={password}
-          error={isSubmit && (validateTextField(password, 1) !== "" || !foundUser)}
-          errorMsg={isSubmit && validateTextField(password, 1)}
+          error={isSubmit && (isPasswordErr || isSupabaseErr)}
+          errorMsg={isSubmit && passwordErrMsg}
         />
 
-        {isSubmit && !foundUser && (
-          <Box sx={login_components} display="flex">
-            <Typography color="error">อีเมลหรือรหัสผ่านไม่ถูกต้อง</Typography>
+        {isSubmit && isSupabaseErr && (
+          <Box sx={login_layout} display="flex">
+            <Typography color="error">{supabaseErrMsg}</Typography>
           </Box>
         )}
 
         <CommonButton label="Login" onClick={handleSubmit} />
 
-        <Box sx={login_components} display="flex">
+        <Box sx={login_layout} display="flex">
           <Typography>Create account{"\u00A0"}</Typography>
           <Link color="primary" underline="hover" href="/register">
             <Typography>here</Typography>
