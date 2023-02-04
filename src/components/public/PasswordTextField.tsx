916add4c1a52a45c673ada7b3e5567ed103333f4
Chinnapat Nitipornsri <kharkkr@gmail.com>
2023-02-04 15:34:08 +0700

    Refactor passwordTextField

---
+++ src/components/public/PasswordTextField.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+import { TextField, InputAdornment, IconButton } from "@mui/material";
+import { Visibility, VisibilityOff } from "@mui/icons-material";
+
+type props = {
+  value: string;
+  handleChange: (event: React.ChangeEvent<HTMLTextAreaElement | HTMLInputElement>) => void;
+  error: boolean;
+  errorMsg?: string | boolean;
+};
+
+export default function PasswordTextFeild(props: props) {
+  const [showPassword, setShowPassword] = React.useState(false);
+  const handleClickShowPassword = () => setShowPassword((show) => !show);
+
+  return (
+    <>
+      <TextField
+        fullWidth
+        label="Password"
+        value={props.value}
+        onChange={(e) => props.handleChange(e)}
+        error={props.error}
+        type={showPassword ? "text" : "password"}
+        InputProps={{
+          endAdornment: (
+            <InputAdornment position="end">
+              <IconButton onClick={handleClickShowPassword} edge="end">
+                {showPassword ? <VisibilityOff /> : <Visibility />}
+              </IconButton>
+            </InputAdornment>
+          ),
+        }}
+      />
+    </>
+  );
+}
