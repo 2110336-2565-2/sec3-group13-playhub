d74ac5001b165b261c387fdab25b85536c567b2f
Chinnapat Nitipornsri <kharkkr@gmail.com>
2023-02-04 11:37:02 +0700

    Enable menu routing

--- src/components/public/Navbar.tsx
+++ src/components/public/Navbar.tsx
@@ -1,17 +1,52 @@
 import React from "react";
-import AppBar from "@mui/material/AppBar";
-import Avatar from "@mui/material/Avatar";
-import Box from "@mui/material/Box";
-import IconButton from "@mui/material/IconButton";
-import Link from "@mui/material/Link";
-import Menu from "@mui/material/Menu";
-import MenuItem from "@mui/material/MenuItem";
-import Toolbar from "@mui/material/Toolbar";
-import Button from "@mui/material/Button";
-import Typography from "@mui/material/Typography";
+import {
+  AppBar,
+  Avatar,
+  Box,
+  IconButton,
+  Link,
+  Menu,
+  MenuItem,
+  Toolbar,
+  Button,
+  Typography,
+} from "@mui/material";
 import Logo from "./Logo";
+import { User } from "@/types/User";
+import { useRouter } from "next/router";
+import { PagePaths } from "enum/pages";
+
+import { page } from "@/types/Page";
+import { NavbarPages } from "enum/navbar";
+
+// mocked user information
+const tmpUser: User = {
+  name: "Chanathip sombuthong",
+  sex: "Male",
+  birthdate: "26/4/2002",
+  description: "ชอบเล่นแนวบลัฟครับ หรือจะไปเล่นห้องผมก็ได้นะ",
+  image: "/images/aom.jpg",
+  email: "aom@gmail.com",
+};
+
+const menuItems: page[] = [
+  {
+    name: NavbarPages.profile,
+    path: PagePaths.profile,
+  },
+  {
+    name: NavbarPages.myPost,
+    path: PagePaths.post,
+  },
+  {
+    name: NavbarPages.logout,
+    path: PagePaths.login,
+  },
+];
 
 export default function Navbar() {
+  const router = useRouter();
+
   const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
 
   const handleClose = () => setAnchorEl(null);
@@ -19,17 +54,25 @@ export default function Navbar() {
     setAnchorEl(event.currentTarget);
   };
 
+  function routeToHome() : void {
+    router.push(PagePaths.home)
+    return ;
+  }
+
   return (
     <>
       <AppBar position="static" color="secondary">
         <Toolbar>
-          <Logo width={50} height={50} />
-          <Button color="inherit">
-            <Typography variant="body1">Home</Typography>
+          <Button color="inherit" disableRipple onClick={routeToHome}>
+            <Logo width={50} height={50} />
           </Button>
-          <Box sx={{ flexGrow: 1 }}></Box>
+          <Box sx={{ flexGrow: 1 }}>
+            <Button color="inherit" disableRipple onClick={routeToHome}>
+              <Typography variant="body1">{NavbarPages.home}</Typography>
+            </Button>
+          </Box>
           <IconButton onClick={handleMenu}>
-            <Avatar alt="Anya" src="/images/aom.jpg" />
+            <Avatar alt="Profile picture" src={tmpUser.image} />
           </IconButton>
           <Menu
             sx={{ mt: "40px" }}
@@ -45,16 +88,18 @@ export default function Navbar() {
             open={Boolean(anchorEl)}
             onClose={handleClose}
           >
-            <MenuItem>
-              <Link textAlign="center" color="inherit" underline="none" href="/profile">
-                Profile
-              </Link>
-            </MenuItem>
-            <MenuItem>
-              <Link textAlign="center" color="error" underline="none" href="/login">
-                Logout
-              </Link>
-            </MenuItem>
+            {menuItems.map((item: page) => (
+              <MenuItem>
+                <Link
+                  textAlign="center"
+                  color={item.name === NavbarPages.logout ? "error" : "inherit"}
+                  underline="none"
+                  href={item.path}
+                >
+                  <Typography variant="body1">{item.name}</Typography>
+                </Link>
+              </MenuItem>
+            ))}
           </Menu>
         </Toolbar>
       </AppBar>
