1e7fb4e1611a952d82e2ef942b42a5176c6e79fc
Tanakit Phentun <tum_tanakid@hotmail.com>
2023-02-08 22:34:06 +0700

    I change almost eveything that you comment to me.

--- src/pages/createPost.tsx
+++ src/pages/createPost.tsx
@@ -8,12 +8,15 @@ import {
   FormHelperText,
   Stack,
   Autocomplete,
+  InputAdornment,
 } from "@mui/material";
 import dayjs, { Dayjs } from "dayjs";
 import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
-import { DateTimePicker } from "@mui/x-date-pickers/DateTimePicker";
+import { MobileDateTimePicker } from "@mui/x-date-pickers/MobileDateTimePicker";
 import PictureList from "@/components/createPost/pictureList";
+import LocationOnIcon from "@mui/icons-material/LocationOn";
+import CalendarTodayIcon from "@mui/icons-material/CalendarToday";
 
 const createPost = () => {
   const error = "#ff0000";
@@ -39,15 +42,36 @@ const createPost = () => {
     backgroundColor: "#ffa31a",
     color: "black",
   };
-
-  const [value, setValue] = React.useState<Dayjs | null>(dayjs("2014-08-18T21:11:54"));
-
-  const handleChange = (newValue: Dayjs | null) => {
-    setValue(newValue);
+  const fontDesign = {
+    fontfamily: "Roboto",
+    fontstyle: "normal",
+    fontweight: "700",
+    fontsize: "20px",
+    lineheight: "24px",
+    letterspacing: "0.15px",
+    color: "#000000",
   };
 
-  const Location = [{ title: "Poseidon entertainment complex" }, { title: "Jod Fair" }];
-  const Tags = [{ Tag: "RPG" }, { Tag: "Car" }, { Tag: "CO-OP" }];
+  const [startDate, setStartDate] = React.useState<Dayjs | null>(dayjs());
+  const [endDate, setEndDate] = React.useState<Dayjs | null>();
+
+  const handleStartDate = (newValue: Dayjs | null) => {
+    if (newValue && newValue.isBefore(dayjs())) {
+      console.error("Start date must be in the future.");
+    } else {
+      setStartDate(newValue);
+    }
+  };
+  const handleEndDate = (newValue: Dayjs | null) => {
+    if (newValue && newValue.isBefore(startDate)) {
+      console.error("End date cannot be before Start Date.");
+    } else {
+      setEndDate(newValue);
+    }
+  };
+
+  const Location = ["Poseidon entertainment complex", "Jod Fair"];
+  const Tags = ["RPG", "Car", "CO-OP"];
 
   return (
     <>
@@ -76,7 +100,9 @@ const createPost = () => {
               placeholder="เช่น หาเพื่อนไปเที่ยวบอร์ดเกม"
             />
             <div style={helperTextBox}>
-              <FormHelperText sx={helperTextError}>ช่องนี้ไม่สามารถเว้นว่างได้</FormHelperText>
+              <FormHelperText sx={helperTextError}>
+                ช่องนี้ไม่สามารถเว้นว่างได้
+              </FormHelperText>
               <FormHelperText sx={helperText}>0/100</FormHelperText>
             </div>
           </Grid>
@@ -86,12 +112,27 @@ const createPost = () => {
               <Autocomplete
                 id="free-solo-demo"
                 freeSolo
-                options={Location.map((option) => option.title)}
-                renderInput={(params) => <TextField {...params} placeholder="เลือกสถานที่" />}
+                options={Location.map((option) => option)}
+                renderInput={(params) => (
+                  <TextField
+                    {...params}
+                    placeholder="เลือกสถานที่"
+                    // Below is for location icon but when i show icon, It can't select location.
+                    /*InputProps={{
+                      startAdornment: (
+                        <InputAdornment position="start">
+                          <LocationOnIcon />
+                        </InputAdornment>
+                      ),
+                    }}*/
+                  />
+                )}
               />
             </Stack>
             <div style={helperTextBox}>
-              <FormHelperText sx={helperTextError}>ช่องนี้ไม่สามารถเว้นว่างได้</FormHelperText>
+              <FormHelperText sx={helperTextError}>
+                ช่องนี้ไม่สามารถเว้นว่างได้
+              </FormHelperText>
               <FormHelperText sx={helperText}>0/100</FormHelperText>
             </div>
           </Grid>
@@ -101,27 +142,53 @@ const createPost = () => {
               <Grid item xs={6}>
                 <Typography variant="body1">Start</Typography>
                 <LocalizationProvider dateAdapter={AdapterDayjs}>
-                  <DateTimePicker
-                    value={value}
-                    onChange={handleChange}
-                    renderInput={(params) => <TextField {...params} fullWidth />}
+                  <MobileDateTimePicker
+                    inputFormat="DD/MM/YYYY hh:mm a"
+                    mask="__/__/____ __:__ _M"
+                    value={startDate}
+                    onChange={handleStartDate}
+                    renderInput={(params) => (
+                      <TextField {...params} fullWidth />
+                    )}
+                    InputProps={{
+                      startAdornment: (
+                        <InputAdornment position="start">
+                          <CalendarTodayIcon />
+                        </InputAdornment>
+                      ),
+                    }}
                   />
                 </LocalizationProvider>
                 <div style={helperTextBox}>
-                  <FormHelperText sx={helperTextError}>ช่องนี้ไม่สามารถเว้นว่างได้</FormHelperText>
+                  <FormHelperText sx={helperTextError}>
+                    ช่องนี้ไม่สามารถเว้นว่างได้
+                  </FormHelperText>
                 </div>
               </Grid>
               <Grid item xs={6}>
                 <Typography variant="body1">End</Typography>
                 <LocalizationProvider dateAdapter={AdapterDayjs}>
-                  <DateTimePicker
-                    value={value}
-                    onChange={handleChange}
-                    renderInput={(params) => <TextField {...params} fullWidth />}
+                  <MobileDateTimePicker
+                    inputFormat="DD/MM/YYYY hh:mm a"
+                    mask="__/__/____ __:__ _M"
+                    value={endDate}
+                    onChange={handleEndDate}
+                    renderInput={(params) => (
+                      <TextField {...params} fullWidth />
+                    )}
+                    InputProps={{
+                      startAdornment: (
+                        <InputAdornment position="start">
+                          <CalendarTodayIcon />
+                        </InputAdornment>
+                      ),
+                    }}
                   />
                 </LocalizationProvider>
                 <div style={helperTextBox}>
-                  <FormHelperText sx={helperTextError}>ช่องนี้ไม่สามารถเว้นว่างได้</FormHelperText>
+                  <FormHelperText sx={helperTextError}>
+                    ช่องนี้ไม่สามารถเว้นว่างได้
+                  </FormHelperText>
                 </div>
               </Grid>
             </Grid>
@@ -133,9 +200,13 @@ const createPost = () => {
               limitTags={5}
               id="multiple-limit-tags"
               options={Tags}
-              getOptionLabel={(option) => option.Tag}
+              getOptionLabel={(option) => option}
               renderInput={(params) => (
-                <TextField {...params} placeholder="Click for add tag" fullWidth />
+                <TextField
+                  {...params}
+                  placeholder="Click for add tag"
+                  fullWidth
+                />
               )}
             />
           </Grid>
@@ -148,20 +219,27 @@ const createPost = () => {
               placeholder="เช่น มาเที่ยวกันเลย ร้านบอร์ดเกมแถวรัชดา"
             />
             <div style={helperTextBox}>
-              <FormHelperText sx={helperTextError}>ช่องนี้ไม่สามารถเว้นว่างได้</FormHelperText>
+              <FormHelperText sx={helperTextError}>
+                ช่องนี้ไม่สามารถเว้นว่างได้
+              </FormHelperText>
               <FormHelperText sx={helperText}>0/500</FormHelperText>
             </div>
           </Grid>
           <Grid item>
             <Typography variant="h5">Image(Optional)</Typography>
-            <Stack direction="row" justifyContent="flex-start" alignItems="flex-start" spacing={1}>
+            <Stack
+              direction="row"
+              justifyContent="flex-start"
+              alignItems="flex-start"
+              spacing={1}
+            >
               <PictureList />
             </Stack>
           </Grid>
         </div>
         <Grid item>
           <Button variant="contained" sx={createPostBtn}>
-            <Typography variant="subtitle1">Create Post</Typography>
+            <Typography style={fontDesign}>Create Post</Typography>
           </Button>
         </Grid>
       </Grid>
