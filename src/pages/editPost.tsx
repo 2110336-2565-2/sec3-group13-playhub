b8580335047f41b85993cf2483e97e245511c6f4
Theerachot <theerachot-tle@hotmail.com>
2023-02-09 17:56:10 +0700

    create image upload still not resize though

--- src/pages/editPost.tsx
+++ src/pages/editPost.tsx
@@ -1,11 +1,30 @@
 import React from "react";
+import { useState, useEffect, useRef } from "react";
 import Navbar from "@/components/public/Navbar";
-import { Grid, Typography, TextField, Button, FormHelperText, Stack, Autocomplete } from "@mui/material";
+import {
+  Grid,
+  Typography,
+  TextField,
+  Button,
+  FormHelperText,
+  Stack,
+  Autocomplete,
+  Box,
+} from "@mui/material";
 import dayjs, { Dayjs } from "dayjs";
 import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 import { DateTimePicker } from "@mui/x-date-pickers/DateTimePicker";
 import PictureList from "@/components/createPost/pictureList";
+import UploadBox from "@/components/public/UploadBox";
+import Image from "next/image";
+import { Post } from "@/types/Post";
+import { styled } from "@mui/material/styles";
+import CancelIcon from "@mui/icons-material/Cancel";
+
+const Input = styled("input")({
+  display: "none",
+});
 
 export default function editPost() {
   const error = "#ff0000";
@@ -42,6 +61,26 @@ export default function editPost() {
   const Location = [{ title: "Poseidon entertainment complex" }, { title: "Jod Fair" }];
   const Tags = [{ Tag: "RPG" }, { Tag: "Car" }, { Tag: "CO-OP" }];
 
+  const [images, setImages] = useState<string[]>([]);
+  const inputRef = useRef<HTMLInputElement>(null);
+  // useEffect(() => {
+  //   setImages(prop.image);
+  // }, [prop.image]);
+
+  const handleAddImages = () => {
+    if (!inputRef || !inputRef.current) return;
+    inputRef.current.click();
+  };
+  const handleImagesInput = async (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (e.target.files && e.target.files.length > 0) {
+      const selectedFile = e.target.files[0];
+      const img = URL.createObjectURL(selectedFile);
+      setImages((prevImg) => prevImg.concat(img));
+    }
+  };
+  const handleDeleteImage = (index: number) => {
+    setImages((prevPics) => prevPics.filter((pic, i) => i !== index));
+  };
   return (
     <>
       <Navbar />
@@ -64,7 +103,12 @@ export default function editPost() {
         <div style={editInfoContainer}>
           <Grid item>
             <Typography variant="h5">Title</Typography>
-            <TextField variant="outlined" fullWidth size="small" placeholder="เช่น หาเพื่อนไปเที่ยวบอร์ดเกม" />
+            <TextField
+              variant="outlined"
+              fullWidth
+              size="small"
+              placeholder="เช่น หาเพื่อนไปเที่ยวบอร์ดเกม"
+            />
             <div style={helperTextBox}>
               <FormHelperText sx={helperTextError}>ช่องนี้ไม่สามารถเว้นว่างได้</FormHelperText>
               <FormHelperText sx={helperText}>0/100</FormHelperText>
@@ -129,7 +173,9 @@ export default function editPost() {
               id="multiple-limit-tags"
               options={Tags}
               getOptionLabel={(option) => option.Tag}
-              renderInput={(params) => <TextField {...params} placeholder="Click for add tag" fullWidth />}
+              renderInput={(params) => (
+                <TextField {...params} placeholder="Click for add tag" fullWidth />
+              )}
             />
           </Grid>
 
@@ -148,9 +194,39 @@ export default function editPost() {
           </Grid>
           <Grid item>
             <Typography variant="h5">Image(Optional)</Typography>
-            <Stack direction="row" justifyContent="flex-start" alignItems="flex-start" spacing={1}>
-              <PictureList />
-            </Stack>
+            <Grid container marginTop={2} spacing={10}>
+              {images ? (
+                <>
+                  {images.map((e, index) => (
+                    <Grid
+                      item
+                      key={index}
+                      style={{ position: "relative", padding: "30px 0px 0px 30px" }}
+                    >
+                      <Image src={e} alt="location" width={300} height={350} />
+                      <CancelIcon
+                        onClick={() => handleDeleteImage(index)}
+                        style={{
+                          fontSize: "30px",
+                          position: "absolute",
+                          top: 0,
+                          right: 0,
+                          color: "grey",
+                        }}
+                      />
+                    </Grid>
+                  ))}
+                </>
+              ) : null}
+              <Grid
+                item
+                sx={{ display: images.length == 3 ? "none" : null }}
+                style={{ padding: "30px 0px 0px 30px" }}
+              >
+                <Input type="file" accept="image/*" ref={inputRef} onChange={handleImagesInput} />
+                <UploadBox onClick={handleAddImages} />
+              </Grid>
+            </Grid>
           </Grid>
         </div>
         <Grid item>
