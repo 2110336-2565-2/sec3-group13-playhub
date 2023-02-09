8e445c544fd3eafe75a9784866c6bcd2889fb630
SoulCRYSIS <ohm9411@gmail.com>
2023-02-10 00:32:21 +0700

    Implement editFrom for fields title, tags, description, location, startDate, endDate

--- src/pages/editPost.tsx
+++ src/pages/editPost.tsx
@@ -1,6 +1,6 @@
 import React from "react";
-import { useState, useEffect, useRef } from "react";
 import Navbar from "@/components/public/Navbar";
+import { useState } from "react";
 import {
   Grid,
   Typography,
@@ -9,27 +9,36 @@ import {
   FormHelperText,
   Stack,
   Autocomplete,
-  Box,
+  InputAdornment,
+  Chip,
 } from "@mui/material";
 import dayjs, { Dayjs } from "dayjs";
 import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
-import { DateTimePicker } from "@mui/x-date-pickers/DateTimePicker";
+import { MobileDateTimePicker } from "@mui/x-date-pickers/MobileDateTimePicker";
 import PictureList from "@/components/createPost/pictureList";
-import UploadBox from "@/components/public/UploadBox";
-import Image from "next/image";
+import LocationOnIcon from "@mui/icons-material/LocationOn";
+import CalendarTodayIcon from "@mui/icons-material/CalendarToday";
 import { Post } from "@/types/Post";
-import { styled } from "@mui/material/styles";
-import CancelIcon from "@mui/icons-material/Cancel";
+import ReactDOM from "react-dom";
 
-const Input = styled("input")({
-  display: "none",
-});
+const dummyEditFrom: Post = {
+  title: "this is title",
+  ownerName: "this is owner name",
+  ownerProfilePic: "",
+  tags: ["A", "B"],
+  description: "this is description",
+  image: [],
+  location: "this is location",
+  time: "",
+};
 
-export default function editPost() {
+const createPost = () => {
+  // for tesing
+  const editFrom: Post = dummyEditFrom;
+  //
   const error = "#ff0000";
-
-  const editInfoContainer = {
+  const createPostContainer = {
     width: "50vw",
     margin: "3vh 0 0 0",
   };
@@ -47,44 +56,57 @@ export default function editPost() {
     textAlign: "end",
     gridColumn: 2,
   };
-  const saveBtn = {
+  /*const createPostBtn = {
     backgroundColor: "#ffa31a",
     color: "black",
+  };*/
+  const fontDesign = {
+    fontstyle: "normal",
+    fontweight: "700",
+    fontsize: "20px",
+    lineheight: "24px",
+    letterspacing: "0.15px",
+    color: "#000000",
   };
 
-  const [value, setValue] = React.useState<Dayjs | null>(dayjs("2014-08-18T21:11:54"));
+  const [startDate, setStartDate] = React.useState<Dayjs | null>(dayjs("2023-01-01"));
+  const [endDate, setEndDate] = React.useState<Dayjs | null>(dayjs("2023-01-02"));
+  const [selectedTags, setSelectedTags] = useState(editFrom.tags ?? []);
 
-  const handleChange = (newValue: Dayjs | null) => {
-    setValue(newValue);
-  };
-
-  const Location = [{ title: "Poseidon entertainment complex" }, { title: "Jod Fair" }];
-  const Tags = [{ Tag: "RPG" }, { Tag: "Car" }, { Tag: "CO-OP" }];
-
-  const [images, setImages] = useState<string[]>([]);
-  const inputRef = useRef<HTMLInputElement>(null);
-  // useEffect(() => {
-  //   setImages(prop.image);
-  // }, [prop.image]);
-
-  const handleAddImages = () => {
-    if (!inputRef || !inputRef.current) return;
-    inputRef.current.click();
-  };
-  const handleImagesInput = async (e: React.ChangeEvent<HTMLInputElement>) => {
-    if (e.target.files && e.target.files.length > 0) {
-      const selectedFile = e.target.files[0];
-      const img = URL.createObjectURL(selectedFile);
-      setImages((prevImg) => prevImg.concat(img));
+  const handleAddTag = (event: any, tag: any) => {
+    if (tag.length <= 5) {
+      console.log(tag.length);
+      setSelectedTags(tag);
     }
   };
-  const handleDeleteImage = (index: number) => {
-    setImages((prevPics) => prevPics.filter((pic, i) => i !== index));
+
+  const handleStartDate = (newValue: Dayjs | null) => {
+    if (newValue && newValue.isBefore(dayjs())) {
+      console.error("Start date must be in the future.");
+    } else {
+      setStartDate(newValue);
+    }
   };
+  const handleEndDate = (newValue: Dayjs | null) => {
+    if (newValue && newValue.isBefore(startDate)) {
+      console.error("End date cannot be before Start Date.");
+    } else {
+      setEndDate(newValue);
+    }
+  };
+
+  const Location = [
+    "Poseidon entertainment complex",
+    "Jod Fair",
+    "Fortune",
+    "The street",
+    "ศูนย์วัฒนธรรมแห่งประเทศไทย",
+  ];
+  const Tags = ["RPG", "Car", "CO-OP", "A", "B", "C", "D"];
+
   return (
     <>
       <Navbar />
-
       <Grid
         container
         direction="column"
@@ -96,11 +118,10 @@ export default function editPost() {
       >
         <Grid item>
           <Typography variant="h1" component="h2">
-            Create post
+            Edit post
           </Typography>
         </Grid>
-
-        <div style={editInfoContainer}>
+        <div style={createPostContainer}>
           <Grid item>
             <Typography variant="h5">Title</Typography>
             <TextField
@@ -108,6 +129,7 @@ export default function editPost() {
               fullWidth
               size="small"
               placeholder="เช่น หาเพื่อนไปเที่ยวบอร์ดเกม"
+              defaultValue={editFrom.title}
             />
             <div style={helperTextBox}>
               <FormHelperText sx={helperTextError}>ช่องนี้ไม่สามารถเว้นว่างได้</FormHelperText>
@@ -116,46 +138,75 @@ export default function editPost() {
           </Grid>
           <Grid item>
             <Typography variant="h5">Location</Typography>
-
             <Stack spacing={2}>
               <Autocomplete
                 id="free-solo-demo"
                 freeSolo
-                options={Location.map((option) => option.title)}
-                renderInput={(params) => <TextField {...params} placeholder="เลือกสถานที่" />}
+                defaultValue={editFrom.location}
+                options={Location.map((option) => option)}
+                renderInput={(params) => (
+                  <TextField
+                    {...params}
+                    placeholder="เลือกสถานที่"
+
+                    // Below is for location icon but when i show icon, It can't select location.
+                    /*InputProps={{
+                      startAdornment: (
+                        <InputAdornment position="start">
+                          <LocationOnIcon />
+                        </InputAdornment>
+                      ),
+                    }}*/
+                  />
+                )}
               />
             </Stack>
-
             <div style={helperTextBox}>
               <FormHelperText sx={helperTextError}>ช่องนี้ไม่สามารถเว้นว่างได้</FormHelperText>
               <FormHelperText sx={helperText}>0/100</FormHelperText>
             </div>
           </Grid>
-
           <Grid item>
             <Typography variant="h5">Date time</Typography>
             <Grid container spacing={2}>
               <Grid item xs={6}>
                 <Typography variant="body1">Start</Typography>
                 <LocalizationProvider dateAdapter={AdapterDayjs}>
-                  <DateTimePicker
-                    value={value}
-                    onChange={handleChange}
+                  <MobileDateTimePicker
+                    inputFormat="DD/MM/YYYY hh:mm a"
+                    mask="__/__/____ __:__ _M"
+                    value={startDate}
+                    onChange={handleStartDate}
                     renderInput={(params) => <TextField {...params} fullWidth />}
+                    InputProps={{
+                      startAdornment: (
+                        <InputAdornment position="start">
+                          <CalendarTodayIcon />
+                        </InputAdornment>
+                      ),
+                    }}
                   />
                 </LocalizationProvider>
                 <div style={helperTextBox}>
                   <FormHelperText sx={helperTextError}>ช่องนี้ไม่สามารถเว้นว่างได้</FormHelperText>
                 </div>
               </Grid>
-
               <Grid item xs={6}>
                 <Typography variant="body1">End</Typography>
                 <LocalizationProvider dateAdapter={AdapterDayjs}>
-                  <DateTimePicker
-                    value={value}
-                    onChange={handleChange}
+                  <MobileDateTimePicker
+                    inputFormat="DD/MM/YYYY hh:mm a"
+                    mask="__/__/____ __:__ _M"
+                    value={endDate}
+                    onChange={handleEndDate}
                     renderInput={(params) => <TextField {...params} fullWidth />}
+                    InputProps={{
+                      startAdornment: (
+                        <InputAdornment position="start">
+                          <CalendarTodayIcon />
+                        </InputAdornment>
+                      ),
+                    }}
                   />
                 </LocalizationProvider>
                 <div style={helperTextBox}>
@@ -164,28 +215,26 @@ export default function editPost() {
               </Grid>
             </Grid>
           </Grid>
-
           <Grid item>
             <Typography variant="h5">Tag</Typography>
             <Autocomplete
               multiple
-              limitTags={5}
-              id="multiple-limit-tags"
               options={Tags}
-              getOptionLabel={(option) => option.Tag}
-              renderInput={(params) => (
-                <TextField {...params} placeholder="Click for add tag" fullWidth />
-              )}
+              value={selectedTags}
+              onChange={handleAddTag}
+              renderInput={(params) => <TextField {...params} placeholder="Click for add tag" fullWidth />}
             />
           </Grid>
-
           <Grid item>
             <Typography variant="h5">Description</Typography>
             <TextField
               variant="outlined"
               fullWidth
+              multiline
+              rows={4}
               size="medium"
               placeholder="เช่น มาเที่ยวกันเลย ร้านบอร์ดเกมแถวรัชดา"
+              defaultValue={editFrom.description}
             />
             <div style={helperTextBox}>
               <FormHelperText sx={helperTextError}>ช่องนี้ไม่สามารถเว้นว่างได้</FormHelperText>
@@ -194,47 +243,19 @@ export default function editPost() {
           </Grid>
           <Grid item>
             <Typography variant="h5">Image(Optional)</Typography>
-            <Grid container marginTop={2} spacing={10}>
-              {images ? (
-                <>
-                  {images.map((e, index) => (
-                    <Grid
-                      item
-                      key={index}
-                      style={{ position: "relative", padding: "30px 0px 0px 30px" }}
-                    >
-                      <Image src={e} alt="location" width={300} height={350} />
-                      <CancelIcon
-                        onClick={() => handleDeleteImage(index)}
-                        style={{
-                          fontSize: "30px",
-                          position: "absolute",
-                          top: 0,
-                          right: 0,
-                          color: "grey",
-                        }}
-                      />
-                    </Grid>
-                  ))}
-                </>
-              ) : null}
-              <Grid
-                item
-                sx={{ display: images.length == 3 ? "none" : null }}
-                style={{ padding: "30px 0px 0px 30px" }}
-              >
-                <Input type="file" accept="image/*" ref={inputRef} onChange={handleImagesInput} />
-                <UploadBox onClick={handleAddImages} />
-              </Grid>
-            </Grid>
+            <Stack direction="row" justifyContent="flex-start" alignItems="flex-start" spacing={1}>
+              <PictureList />
+            </Stack>
           </Grid>
         </div>
         <Grid item>
-          <Button variant="contained" sx={saveBtn}>
-            <Typography variant="subtitle1">Create Post</Typography>
+          <Button variant="contained" color="primary">
+            <Typography style={fontDesign}>Create Post</Typography>
           </Button>
         </Grid>
       </Grid>
     </>
   );
-}
+};
+
+export default createPost;
