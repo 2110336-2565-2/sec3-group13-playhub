8ef872e0ef4bb98ceb4fbf5ab63fe7f397b5442d
Chinnapat Nitipornsri <kharkkr@gmail.com>
2023-02-16 18:02:12 +0700

    Refactor picturelist

--- src/components/createPost/pictureList.tsx
+++ src/components/createPost/pictureList.tsx
@@ -1,42 +1,57 @@
-import React, { useState, useRef, useEffect } from "react";
+import { useState, useRef, useEffect } from "react";
 import ImageList from "@mui/material/ImageList";
 import ImageListItem from "@mui/material/ImageListItem";
 import CancelIcon from "@mui/icons-material/Cancel";
 import AddIcon from "@mui/icons-material/Add";
-import Image from "next/image";
+import {
+  Box,
+  Container,
+  FormHelperText,
+  IconButton,
+  Stack,
+  Typography,
+} from "@mui/material";
+import { grey } from "@mui/material/colors";
+import { IMAGE_LIMIT } from "enum/inputLimit";
+import { validation } from "@/types/Validation";
 
+// style
+const image_layout = {
+  width: "10vw",
+  minWidth: "140px",
+  height: "10vw",
+  minHeight: "140px",
+};
+
+const addIcon_layout = {
+  width: "6vw",
+  minWidth: "90px",
+  height: "6vw",
+  minHeight: "90px",
+};
+
+// type
 type props = {
+  header?: string;
+  note?: string;
   imgs: string[];
-  stateChanger: React.Dispatch<React.SetStateAction<string[]>>;
-  st: React.Dispatch<React.SetStateAction<boolean>>;
-};
-const cancleDesign = {
-  position: "absolute",
-  top: 0,
-  right: 0,
-  cursor: "pointer",
-  color: "red",
-};
-const addBoxDesign = {
-  width: "200px",
-  height: "200px",
-  border: "2px dashed black",
-  display: "flex",
-  alignItems: "center",
-  justifyContent: "center",
-  boxshadow: "0px 4px 4px rgba(0, 0, 0, 0.25)",
+  stateChanger: (images: string[]) => void;
+  isErr: boolean;
+  errMsg: string;
 };
 
-const PictureList = (props: props) => {
-  const [dataPics, setDataPics] = useState<string[]>([]);
+export default function PictureList(props: props) {
+  const fileInputRef = useRef<HTMLInputElement>(null);
+  const [images, setImages] = useState<string[]>([]);
 
   useEffect(() => {
-    setDataPics(props.imgs);
+    setImages(props.imgs);
   }, [props.imgs]);
+
   const deletePicture = (index: number) => {
-    setDataPics((prevPics) => prevPics.filter((pic, i) => i !== index));
-    props.stateChanger(dataPics.filter((pic, i) => i !== index));
-    props.st(false);
+    setImages((prevPics) => prevPics.filter((pic, i) => i !== index));
+    props.stateChanger(images.filter((pic, i) => i !== index));
+    // props.st(false);
   };
 
   const handleAddPicture = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -47,21 +62,20 @@ const PictureList = (props: props) => {
         file.type.startsWith("image/")
       );
 
-      if (selfilesarr.length + dataPics.length > 3) {
+      if (selfilesarr.length + images.length > 3) {
         //alert("more than 3");
-        props.st(true);
+        // props.st(true);
         return;
       }
 
       const imgArr = selfilesarr.map((file) => {
         return URL.createObjectURL(file);
       });
-      setDataPics((prevImg) => prevImg.concat(imgArr));
-      props.stateChanger(dataPics.concat(imgArr));
-      props.st(false);
+      setImages((prevImg) => prevImg.concat(imgArr));
+      props.stateChanger(images.concat(imgArr));
+      // props.st(false);
     }
   };
-  const fileInputRef = useRef<HTMLInputElement>(null);
 
   const handleClick = () => {
     if (!fileInputRef || !fileInputRef.current) return;
@@ -70,29 +84,76 @@ const PictureList = (props: props) => {
 
   return (
     <>
-      <ImageList cols={4} rowHeight={200}>
-        {dataPics.map((image, index) => (
-          <ImageListItem key={index}>
-            <CancelIcon sx={cancleDesign} onClick={() => deletePicture(index)}>
-              {" "}
-            </CancelIcon>
-            <Image
-              alt=""
-              src={`${image}`}
-              loading="lazy"
-              width="200"
-              height="200"
-            />
-          </ImageListItem>
-        ))}
+      <Box display="flex">
+        <Typography variant="body1">
+          {props.header}
+          {"\u00A0"}
+        </Typography>
+        <Typography variant="body1" sx={{ color: grey[500] }}>
+          {props.note}
+        </Typography>
+      </Box>
 
-        {dataPics.length < 3 ? (
-          <div style={addBoxDesign} onClick={handleClick}>
-            <AddIcon style={{ width: "100px", height: "100px" }} />
-          </div>
-        ) : null}
-      </ImageList>
+      <Container disableGutters>
+        <ImageList
+          variant="woven"
+          cols={Math.min(3, images.length + 2)}
+          gap={10}
+        >
+          {images.map((image, index) => {
+            // const imageErr :validation = validateImage(image.type, image.size);
+            return (
+              <>
+                <Stack>
+                  <Box key={index} sx={image_layout}>
+                    <ImageListItem cols={1}>
+                      <IconButton
+                        color="error"
+                        sx={{
+                          position: "absolute",
+                          top: 0,
+                          right: 0,
+                          padding: 0,
+                        }}
+                        onClick={() => deletePicture(index)}
+                      >
+                        <CancelIcon />
+                      </IconButton>
+                      <img
+                        alt="Post_Image"
+                        src={image}
+                        style={{ objectFit: "contain" }}
+                      />
+                    </ImageListItem>
+                  </Box>
+                  {false && (
+                    <FormHelperText error>{props.errMsg}</FormHelperText>
+                  )}
+                </Stack>
+              </>
+            );
+          })}
+          {images.length < IMAGE_LIMIT.MAX_IMAGE && (
+            <Box key={4} sx={image_layout}>
+              <ImageListItem cols={1}>
+                <IconButton
+                  color="secondary"
+                  sx={{
+                    borderRadius: 0,
+                    border: "2px dashed black",
+                    ...image_layout,
+                  }}
+                  onClick={handleClick}
+                >
+                  <AddIcon style={addIcon_layout} />
+                </IconButton>
+              </ImageListItem>
+            </Box>
+          )}
+        </ImageList>
+      </Container>
 
+      {false && <FormHelperText error>{props.errMsg}</FormHelperText>}
       <input
         ref={fileInputRef}
         hidden
@@ -103,6 +164,4 @@ const PictureList = (props: props) => {
       />
     </>
   );
-};
-
-export default PictureList;
+}
