250954ff63031816ccf201553b22591987bf3045
Aom <oommankung@gmail.com>
2023-02-22 20:16:18 +0700

    change message error to constant

---
+++ src/constants/supabase.ts
@@ -0,0 +1,3 @@
+export const SUPABASE_LOGIN_EMAIL_NOT_VALIDATED_ERROR = "Email not confirmed"
+export const SUPABASE_LOGIN_CREDENTIALS_ERROR = "Invalid login credentials"
+export const SUPABASE_CONNECTING_ERROR = "Can't connect to Supabase"
