7ecd70ce84b120cdac4201d7138a869241faf543
Nichapat Kerkkittikul <palm.nichapat@gmail.com>
2023-02-22 12:14:43 +0700

    Fix: hydration UI

--- src/pages/verify.tsx
+++ src/pages/verify.tsx
@@ -19,18 +19,18 @@ export default function Verify() {
             >
                 <Box>
                     <Typography gutterBottom variant="h1">Verify คืออะไร</Typography>
-                    <Typography variant="body1">คือ ระบบการยืนยันตัวตน เพื่อสร้างความอุ่นใจและปลอดภัยแก่ท่านและสมาชิกชาว PlayHub ที่เข้ามาใช้บริการเว็บไซต์ของเรา รวมไปถึงการได้รับสิทธิพิเศษต่าง ๆ จาก PlayHub</Typography>
+                    <Typography component="span" variant="body1">คือ ระบบการยืนยันตัวตน เพื่อสร้างความอุ่นใจและปลอดภัยแก่ท่านและสมาชิกชาว PlayHub ที่เข้ามาใช้บริการเว็บไซต์ของเรา รวมไปถึงการได้รับสิทธิพิเศษต่าง ๆ จาก PlayHub</Typography>
                 </Box>
 
                 <Box>
                     <Typography gutterBottom variant="h1">Verify แล้วได้อะไร</Typography>
-                    <Typography variant="body1">ท่านจะสามารถสร้างกิจกรรมเพื่อหาเพื่อนๆมาร่วมสนุกกับท่านได้
+                    <Typography component="span" variant="body1">ท่านจะสามารถสร้างกิจกรรมเพื่อหาเพื่อนๆมาร่วมสนุกกับท่านได้
                         ท่านจะได้รับเครื่องหมายยืนยันตัวตนสุดเท่ในหน้าโปรไฟล์ของท่าน</Typography>
                 </Box>
 
                 <Box>
                     <Typography gutterBottom variant="h1">Verify อย่างไร</Typography>
-                    <Typography variant="body1">
+                    <Typography component="span" variant="body1">
                         ส่ง
                         <Link href="/" style={{ color: "blue", textDecorationLine: "underline" }}>
                             อีเมล
@@ -44,7 +44,7 @@ export default function Verify() {
                         <Chip
                             icon={<VerifiedIcon sx={{ color: "white" }} />}
                             color="primary"
-                            label={"Verified"}
+                            label="Verified"
                         />
                         &nbsp;
                         ปรากฎขึ้นในหน้าโปร์ไฟล์ของท่าน
