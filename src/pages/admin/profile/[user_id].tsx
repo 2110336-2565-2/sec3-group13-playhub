cc2667e5496d3a1e68427bc5059f58787d65c465
Nichapat Kerkkittikul <palm.nichapat@gmail.com>
2023-02-24 22:39:45 +0700

    Initialize profile page for admin perspective

---
+++ src/pages/admin/profile/[user_id].tsx
@@ -0,0 +1,111 @@
+import { Suspense, useContext, useEffect, useState } from "react";
+
+import { NextRouter, useRouter } from "next/router";
+
+import { userContext } from "supabase/user_context";
+import { useSupabaseClient } from "@supabase/auth-helpers-react";
+import { Database } from "supabase/db_types";
+
+import { Avatar, Chip, Typography, Stack } from "@mui/material";
+import MaleIcon from "@mui/icons-material/Male";
+import FemaleIcon from "@mui/icons-material/Female";
+import TransgenderIcon from "@mui/icons-material/Transgender";
+import CakeIcon from "@mui/icons-material/Cake";
+
+import Navbar from "@/components/admin/AdminNavbar";
+import Loading from "@/components/public/Loading";
+
+import { User } from "@/types/User";
+import { PagePaths } from "enum/pages";
+import { Gender } from "enum/gender";
+import { GetUserByUserId } from "@/services/User";
+
+// style
+const profile_layout = {
+    width: "30vw",
+    minWidth: "200px",
+};
+const avatar = { width: 200, height: 200 };
+
+export default function Home() {
+    const router: NextRouter = useRouter();
+    const userStatus = useContext(userContext);
+    const supabaseClient = useSupabaseClient<Database>();
+
+    const [targetUserData, setTargetUserData] = useState<User | null>(null);
+
+    useEffect(() => {
+        async function getTargetUserData() {
+            if (!userStatus.user || !router.query.user_id || targetUserData) return;
+            const userData = await GetUserByUserId(router.query.user_id as string, supabaseClient);
+            setTargetUserData(userData);
+        }
+
+        getTargetUserData();
+    }, [router.query.user_id, supabaseClient, userStatus.user, targetUserData]);
+
+    if (userStatus.isLoading) return <Loading />;
+    if (!userStatus.user) {
+        router.push(PagePaths.login);
+        return;
+    }
+    if (!userStatus.user.isAdmin) {
+        router.push(PagePaths.home)
+        return;
+    }
+    if (!targetUserData) return <Loading />;
+    return (
+        <>
+            <Suspense fallback={<Loading />}>
+                <Navbar />
+                <Stack
+                    spacing={2}
+                    alignItems="center"
+                    justifyContent="center"
+                    style={{ minHeight: "90vh" }}
+                >
+                    <Typography variant="h1" align="center" sx={profile_layout}>
+                        {targetUserData.username}
+                    </Typography>
+                    {router.query.user_id === userStatus.user.userId && (
+                        <Typography variant="body1" align="center" sx={profile_layout}>
+                            {targetUserData.email}
+                        </Typography>
+                    )}
+                    <Avatar
+                        sx={avatar}
+                        alt="Profile picture"
+                        src={targetUserData.image as string}
+                    />
+                    <Stack direction="row" spacing={1}>
+                        <Chip
+                            icon={
+                                targetUserData.sex === Gender.male ? (
+                                    <MaleIcon />
+                                ) : targetUserData.sex === Gender.female ? (
+                                    <FemaleIcon />
+                                ) : targetUserData.sex === Gender.others ? (
+                                    <TransgenderIcon />
+                                ) : (
+                                    <div></div>
+                                )
+                            }
+                            label={targetUserData.sex}
+                        />
+                        <Chip icon={<CakeIcon />} label={targetUserData.birthdate} />
+                    </Stack>
+
+                    {targetUserData.description.split("\n").map((row) => (
+                        <Typography
+                            variant="body1"
+                            sx={{ ...profile_layout, wordBreak: "break-word" }}
+                            key={row}
+                        >
+                            {row}
+                        </Typography>
+                    ))}
+                </Stack>
+            </Suspense>
+        </>
+    );
+}
