a9dce01fa074ced7cfb163663adedaea3febe8cc
SoulCRYSIS <ohm9411@gmail.com>
2023-02-24 20:14:14 +0700

    delete test files

--- src/pages/testAppointmentCard.tsx
+++
@@ -1,6 +0,0 @@
-import AppointmentCard from "@/components/appointment/AppointmentCard";
-import {testAppointment } from "@/types/Appointment";
-
-export default function Home() {
-    return <AppointmentCard appointment={testAppointment} />;
-}
