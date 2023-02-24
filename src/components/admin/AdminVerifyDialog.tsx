d9f0fc3ea0a76553dde4ba254ce13b4c72e3fbdf
Nichapat Kerkkittikul <palm.nichapat@gmail.com>
2023-02-24 22:58:34 +0700

    Admin: Initialize verify dialog

---
+++ src/components/admin/AdminVerifyDialog.tsx
@@ -0,0 +1,44 @@
+import {
+    Button,
+    Dialog,
+    DialogActions,
+    DialogContent,
+    DialogTitle,
+    Typography,
+} from "@mui/material";
+
+type props = {
+    openModal: boolean;
+    handleCloseModal: () => void;
+    verifyUser: () => void;
+};
+
+export default function AdminVerifyDialog(props: props) {
+    return (
+        <>
+            <Dialog
+                fullWidth={true}
+                maxWidth="sm"
+                open={props.openModal}
+                onClose={props.handleCloseModal}
+            >
+                <DialogTitle>
+                    <Typography>Verify this user</Typography>
+                </DialogTitle>
+                <DialogContent dividers>
+                    <Typography>
+                        เลขบัตรประจำตัวประชาชน
+                    </Typography>
+                </DialogContent>
+                <DialogActions>
+                    <Button variant="contained" color="inherit" onClick={props.handleCloseModal}>
+                        Cancel
+                    </Button>
+                    <Button variant="contained" color="primary" onClick={props.verifyUser}>
+                        Confirm
+                    </Button>
+                </DialogActions>
+            </Dialog>
+        </>
+    );
+}
--- src/pages/admin/profile/[user_id].tsx
+++ src/pages/admin/profile/[user_id].tsx
@@ -6,7 +6,7 @@ import { userContext } from "supabase/user_context";
 import { useSupabaseClient } from "@supabase/auth-helpers-react";
 import { Database } from "supabase/db_types";
 
-import { Avatar, Chip, Typography, Stack } from "@mui/material";
+import { Avatar, Chip, Typography, Stack, Button, Modal } from "@mui/material";
 import MaleIcon from "@mui/icons-material/Male";
 import FemaleIcon from "@mui/icons-material/Female";
 import TransgenderIcon from "@mui/icons-material/Transgender";
@@ -19,6 +19,7 @@ import { User } from "@/types/User";
 import { PagePaths } from "enum/pages";
 import { Gender } from "enum/gender";
 import { GetUserByUserId } from "@/services/User";
+import AdminVerifyDialog from "@/components/admin/AdminVerifyDialog";
 
 // style
 const profile_layout = {
@@ -27,12 +28,14 @@ const profile_layout = {
 };
 const avatar = { width: 200, height: 200 };
 
-export default function Home() {
+export default function adminProfile() {
     const router: NextRouter = useRouter();
     const userStatus = useContext(userContext);
     const supabaseClient = useSupabaseClient<Database>();
 
     const [targetUserData, setTargetUserData] = useState<User | null>(null);
+
+    const [isVerifyModalShow, setIsVerifyModalShow] = useState<boolean>(false);
 
     useEffect(() => {
         async function getTargetUserData() {
@@ -43,6 +46,17 @@ export default function Home() {
 
         getTargetUserData();
     }, [router.query.user_id, supabaseClient, userStatus.user, targetUserData]);
+
+    function verifyUser(): void {
+        console.log("confirm button is clicked!!")
+    }
+
+    function openVerifyModal(): void {
+        setIsVerifyModalShow(true)
+    }
+    function closeVerifyModal(): void {
+        setIsVerifyModalShow(false)
+    }
 
     if (userStatus.isLoading) return <Loading />;
     if (!userStatus.user) {
@@ -104,6 +118,12 @@ export default function Home() {
                             {row}
                         </Typography>
                     ))}
+                    <Button onClick={openVerifyModal} variant="contained">Verify</Button>
+                    <AdminVerifyDialog
+                        openModal={isVerifyModalShow}
+                        handleCloseModal={closeVerifyModal}
+                        verifyUser={verifyUser}
+                    />
                 </Stack>
             </Suspense>
         </>
