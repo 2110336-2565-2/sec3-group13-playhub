7529d140e8fd803feb996ea336f5b544e6d91d63
Tanakit Phentun <tum_tanakid@hotmail.com>
2023-03-01 00:58:18 +0700

    Task To Do handleSubmit button (because it is in this component) what to handle and sending to [createAppointment].tsx 1.isSubmit 2.participant(menuItems in AddParticipamt)

--- src/components/createAppointment/RightCard.tsx
+++ src/components/createAppointment/RightCard.tsx
@@ -6,48 +6,25 @@ import React, { Fragment } from "react";
 
 import { AppointmentDetail } from "@/types/Appointment";
 import { CardContent, Card, Grid, Stack, Typography, Box, Chip, Button } from "@mui/material";
-import LocationOnIcon from "@mui/icons-material/LocationOn";
-import CalendarTodayIcon from "@mui/icons-material/CalendarToday";
-import TagComponent from "../public/TagComponent";
+
 import { useEffect, useState, useContext, ChangeEvent } from "react";
-import { Dayjs } from "dayjs";
+
 import Loading from "../public/Loading";
 import { useRouter } from "next/router";
 
 import { userContext } from "supabase/user_context";
-import { useSupabaseClient } from "@supabase/auth-helpers-react";
-import { Database } from "supabase/db_types";
+
 import { PagePaths } from "enum/pages";
 import { User } from "@/types/User";
 
-const chipsStyle = {
-  minWidth: 100,
-  height: 40,
-  fontSize: 18,
-  fontWeight: "bold",
-  border: "black solid 4px",
-  borderRadius: "16px",
-  boxShadow: "4px 4px #BFBFBF",
-};
-const textboxShadow = {
-  border: "solid 4px",
-  borderRadius: "16px",
-  boxShadow: "8px 8px #BFBFBF",
-};
-const textinBox = {
-  display: "inline-flex",
-  height: "50px",
-  alignItems: "center",
-  paddingLeft: "8px",
-};
-
 type props = {
   rightSideData: AppointmentDetail;
+  /*sendParticipant: User[];
+  sendIsSubmit: Boolean;*/
 };
 export default function RightCard(props: props) {
   const router = useRouter();
   const userStatus = useContext(userContext);
-  const supabaseClient = useSupabaseClient<Database>();
 
   const [participant, setParticipant] = useState<User[]>(props.rightSideData.acceptParticipants);
   const participantError: boolean = participant.length === 0;
@@ -67,19 +44,34 @@ export default function RightCard(props: props) {
   if (userStatus.isLoading) return <Loading />;
   if (!userStatus.user) {
     router.push(PagePaths.login);
-    return;
+    return null;
   }
   if (userStatus.user.isAdmin) {
     router.push(PagePaths.adminHome + userStatus.user.userId);
-    return;
+    return null;
   }
   if (userStatus.isLoading) return <Loading />;
+
+  function handleSubmit() {
+    if (!participantError) {
+      /*
+      ??? Please do this ???
+      check participant first  then send submittatus(isSubmit) and participant to [createAppointment].tsx
+      
+      */
+    }
+  }
 
   return (
     <>
       <BorderWithShadow>
         <CardContent>
-          <Stack spacing="12px" sx={{ padding: "20px", height: "100%" }}>
+          <Stack
+            spacing="12px"
+            sx={{ padding: "20px", height: "100%" }}
+            style={{ position: "relative" }}
+          >
+            {/* Image  */}
             <Box>
               <Typography variant="h5" sx={{ fontWeight: "bold", marginBottom: "4px" }}>
                 Image
@@ -92,7 +84,7 @@ export default function RightCard(props: props) {
                 ))}
               </Grid>
             </Box>
-
+            {/* AddParticipant */}
             <Box>
               <AddParticipant
                 header="Select Participants"
@@ -104,16 +96,17 @@ export default function RightCard(props: props) {
                 errMsg="(เลือกอย่างน้อย 1 คน)"
               />
             </Box>
+            <Box display="flex" justifyContent="center" alignItems="center">
+              <Button
+                variant="contained"
+                style={{ position: "absolute", bottom: 0 }}
+                onClick={handleSubmit}
+              >
+                Create
+              </Button>
+            </Box>
           </Stack>
         </CardContent>
-        <Box display="flex" justifyContent="center" alignItems="center">
-          <Button
-            variant="contained"
-            style={{ position: "absolute", bottom: 0 }} /*onClick={handleSubmit}*/
-          >
-            Create
-          </Button>
-        </Box>
       </BorderWithShadow>
     </>
   );
