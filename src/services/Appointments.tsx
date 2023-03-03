0fe776ab71bca596fe36c9a6c0af814fecdba26f
Chinnapat Nitipornsri <kharkkr@gmail.com>
2023-03-03 21:40:42 +0700

    Implement createAppointment endpoint

---
+++ src/services/Appointments.tsx
@@ -0,0 +1,29 @@
+import { User } from "@/types/User";
+import { SupabaseClient } from "@supabase/supabase-js";
+import { Database } from "supabase/db_types";
+import { PostInfo } from "../types/Post";
+
+export async function CreateAppointment(
+  post: PostInfo,
+  pending_partipants: User[],
+  accept_participants: User[],
+  supabaseClient: SupabaseClient<Database>
+) {
+  const addAppointmentResult = await supabaseClient.rpc("create_appointment", {
+    title: post.title,
+    location: post.location,
+    description: post.description,
+    tags: post.tags.map((e) => e.id),
+    start_time: post.startTime.toString(),
+    end_time: post.endTime.toString(),
+    pending_user_id: pending_partipants.map((p) => p.userId),
+    accept_user_id: accept_participants.map((p) => p.userId),
+    images: post.images,
+    owner_id: post.userId,
+  });
+
+  if (addAppointmentResult.error) {
+    console.log(addAppointmentResult.error);
+    throw new Error("Something went wrong!!");
+  }
+}
