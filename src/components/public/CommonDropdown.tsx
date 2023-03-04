3b5fc155a29f2291d2e80f5b4d6b1142161e531b
Chinnapat Nitipornsri <kharkkr@gmail.com>
2023-03-04 14:12:00 +0700

    Dynamic start icon on selected gender

--- src/components/public/CommonDropdown.tsx
+++ src/components/public/CommonDropdown.tsx
@@ -6,10 +6,15 @@ import {
   MenuItem,
   Select,
   SelectChangeEvent,
+  Stack,
   Typography,
 } from "@mui/material";
-import LockIcon from "@mui/icons-material/Lock";
 import { grey } from "@mui/material/colors";
+import { Gender } from "enum/gender";
+import MaleIcon from "@mui/icons-material/Male";
+import FemaleIcon from "@mui/icons-material/Female";
+import TransgenderIcon from "@mui/icons-material/Transgender";
+import FavoriteIcon from "@mui/icons-material/Favorite";
 
 type props = {
   header?: string;
@@ -28,47 +33,59 @@ const helperText = {
 };
 
 export default function CommonDropdown(props: props) {
+  function displayGenderIcon(gender: string) {
+    if (gender === Gender.male) {
+      return <MaleIcon fontSize="large" color={props.isErr ? "error" : "secondary"} />;
+    } else if (gender === Gender.female) {
+      return <FemaleIcon fontSize="large" color={props.isErr ? "error" : "secondary"} />;
+    } else if (gender === Gender.others) {
+      return <TransgenderIcon fontSize="large" color={props.isErr ? "error" : "secondary"} />;
+    } else {
+      return <FavoriteIcon fontSize="large" color={props.isErr ? "error" : "secondary"} />;
+    }
+  }
+
   return (
     <>
-      <Typography variant="body1">{props.header}</Typography>
-      <FormControl error={props.isErr} sx={{ backgroundColor: "#ffffff" }} fullWidth>
-        <Select
-          value={props.value}
-          onChange={props.handleValueChange}
-          displayEmpty
-          renderValue={
-            props.value !== ""
-              ? () => <Typography lineHeight={1.4}>{props.value}</Typography>
-              : () => (
-                  <Typography lineHeight={1.4} color={grey[400]}>
-                    {props.placeHolder}
-                  </Typography>
-                )
-          }
-          inputProps={{
-            sx: {
-              textAlign: "center",
-              "&::placeholder": {
+      <Stack spacing={1}>
+        <Typography variant="body1">{props.header}</Typography>
+        <FormControl error={props.isErr} sx={{ backgroundColor: "#ffffff" }} fullWidth>
+          <Select
+            value={props.value}
+            onChange={props.handleValueChange}
+            displayEmpty
+            renderValue={
+              props.value !== ""
+                ? () => <Typography lineHeight={1.4}>{props.value}</Typography>
+                : () => (
+                    <Typography lineHeight={1.4} color={grey[400]}>
+                      {props.placeHolder}
+                    </Typography>
+                  )
+            }
+            inputProps={{
+              sx: {
                 textAlign: "center",
+                "&::placeholder": {
+                  textAlign: "center",
+                },
               },
-            },
-          }}
-          startAdornment={
-            <InputAdornment position="start">
-              <LockIcon fontSize="large" color={props.isErr ? "error" : "secondary"} />
-            </InputAdornment>
-          }
-        >
-          {props.items.map((item) => (
-            <MenuItem key={item} value={item}>
-              {item}
-            </MenuItem>
-          ))}
-        </Select>
-      </FormControl>
-      <Box sx={helperText}>
-        {props.isErr && <FormHelperText error>{props.errMsg}</FormHelperText>}
-      </Box>
+            }}
+            startAdornment={
+              <InputAdornment position="start">{displayGenderIcon(props.value)}</InputAdornment>
+            }
+          >
+            {props.items.map((item) => (
+              <MenuItem key={item} value={item}>
+                {item}
+              </MenuItem>
+            ))}
+          </Select>
+        </FormControl>
+        <Box sx={helperText}>
+          {props.isErr && <FormHelperText error>{props.errMsg}</FormHelperText>}
+        </Box>
+      </Stack>
     </>
   );
 }
