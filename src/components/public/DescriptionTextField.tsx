52a012f97962b478a8e4b24bc21c2f3c19f587c3
Chinnapat Nitipornsri <kharkkr@gmail.com>
2023-03-05 20:32:18 +0700

    Implement description field

---
+++ src/components/public/DescriptionTextField.tsx
@@ -0,0 +1,61 @@
+import {
+  Box,
+  FormHelperText,
+  Icon,
+  InputAdornment,
+  Stack,
+  TextField,
+  Typography,
+} from "@mui/material";
+import EmailIcon from "@mui/icons-material/Email";
+import EditIcon from "@mui/icons-material/Edit";
+import { Icons } from "enum/icons";
+
+type props = {
+  header: string;
+  placeholder: string;
+  value: string;
+  handleValueChange: (event: React.ChangeEvent<HTMLTextAreaElement | HTMLInputElement>) => void;
+  char_limit: number;
+  isErr: boolean;
+  errMsg: string;
+};
+
+const helperText = {
+  display: "flex",
+  flexDirection: "row-reverse",
+  justifyContent: "space-between",
+};
+
+export default function NormalTextField(props: props) {
+  const exceedChar: boolean = props.char_limit ? props.value.length > props.char_limit : false;
+
+  return (
+    <>
+      <Stack spacing={1}>
+        <Box>
+          <Typography variant="body1">{props?.header}</Typography>
+          <TextField
+            sx={{ backgroundColor: "#ffffff" }}
+            fullWidth
+            multiline
+            rows={4}
+            placeholder={props.placeholder}
+            value={props.value}
+            error={props.isErr}
+            onChange={props.handleValueChange}
+          />
+        </Box>
+        <Box sx={helperText}>
+          <FormHelperText error={exceedChar}>
+            {props.char_limit && `${props.value.length}/${props.char_limit}`}
+          </FormHelperText>
+          <FormHelperText error>
+            {props.isErr && props.errMsg}
+            {"\u00A0"}
+          </FormHelperText>
+        </Box>
+      </Stack>
+    </>
+  );
+}
