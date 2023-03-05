0706e6b7224ddea58093e6ed24f9f5f5a4dca716
Chinnapat Nitipornsri <kharkkr@gmail.com>
2023-03-05 20:32:43 +0700

    Fix description size

--- src/pages/profile/[user_id].tsx
+++ src/pages/profile/[user_id].tsx
@@ -143,7 +143,7 @@ export default function Home() {
                 {`" ${targetUserData.description} "`.split("\n").map((row, index) => (
                   <Typography
                     variant="body1"
-                    sx={{ maxWidth: "20vw", wordBreak: "break-word", textAlign: "center" }}
+                    sx={{ maxWidth: "30vw", wordBreak: "break-word", textAlign: "center" }}
                     key={index}
                   >
                     {row}
