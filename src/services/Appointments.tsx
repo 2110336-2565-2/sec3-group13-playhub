5c0c870a349148def9949c7f124144aa02a92003
Aom <oommankung@gmail.com>
2023-03-10 16:16:15 +0700

    add relate file

--- src/services/Appointments.tsx
+++ src/services/Appointments.tsx
@@ -1,5 +1,6 @@
-import { Appointment } from "@/types/Appointment";
+import { Appointment, AppointmentDetail, AppointmentDetailHeader } from "@/types/Appointment";
 import { SupabaseClient } from "@supabase/supabase-js";
+import dayjs from "dayjs";
 import { Database } from "supabase/db_types";
 
 export async function AcceptAppointment(
@@ -40,8 +41,8 @@ export async function RejectAppointment(
 
 export async function GetAppointments(
   supabaseClient: SupabaseClient<Database>
-): Promise<Appointment[]>{
-  const GetAppointmentsResult = await supabaseClient.rpc("get_appointments") ;
+): Promise<Appointment[]> {
+  const GetAppointmentsResult = await supabaseClient.rpc("get_appointments");
 
   if (GetAppointmentsResult.error) {
     console.log(GetAppointmentsResult.error);
@@ -49,6 +50,7 @@ export async function GetAppointments(
   }
 
   return GetAppointmentsResult.data.map((appointment) => ({
+    appointmentId: appointment.id,
     title: appointment.title,
     ownerId: appointment.owner_id,
     ownerName: appointment.username,
@@ -56,6 +58,75 @@ export async function GetAppointments(
     location: appointment.location,
     startDateTime: appointment.start_time,
     endDateTime: appointment.end_time,
-    participantAmount: appointment.participant_number
-  }))
+    participantAmount: appointment.participant_number,
+  }));
+}
+
+export async function GetAppointmentsByAppointmentId(
+  appointmentId: number,
+  supabaseClient: SupabaseClient<Database>
+): Promise<AppointmentDetail> {
+  const getAppointmentsResult = await supabaseClient.rpc("get_appointments_by_appointment_id", {
+    id: appointmentId,
+  });
+
+  if (getAppointmentsResult.error) {
+    console.log(getAppointmentsResult.error);
+    throw new Error("Something went wrong!!");
+  }
+
+  const hostIndex = getAppointmentsResult.data[0].accept_user_names.indexOf(
+    getAppointmentsResult.data[0].username
+  );
+  [
+    getAppointmentsResult.data[0].accept_user_names[0],
+    getAppointmentsResult.data[0].accept_user_names[hostIndex],
+  ] = [
+    getAppointmentsResult.data[0].accept_user_names[hostIndex],
+    getAppointmentsResult.data[0].accept_user_names[0],
+  ];
+
+  const detailHeader: AppointmentDetailHeader = {
+    title: getAppointmentsResult.data[0].title,
+    location: getAppointmentsResult.data[0].location,
+    startDateTime: dayjs(getAppointmentsResult.data[0].start_time),
+    endDateTime: dayjs(getAppointmentsResult.data[0].end_time),
+    tags: getAppointmentsResult.data[0].tags,
+    description: getAppointmentsResult.data[0].description,
+  };
+  return {
+    detailHeader,
+    ownerId: getAppointmentsResult.data[0].owner_id,
+    images: getAppointmentsResult.data[0].images,
+    participantAmount: getAppointmentsResult.data[0].participant_number,
+    pendingParticipants: getAppointmentsResult.data[0].pending_user_names,
+    acceptParticipants: getAppointmentsResult.data[0].accept_user_names,
+    rejectParticipants: getAppointmentsResult.data[0].reject_user_names,
+  };
+}
+
+export async function GetAppointmentsByUserId(
+  userId: string,
+  supabaseClient: SupabaseClient<Database>
+): Promise<Appointment[]> {
+  const getAppointmentsResult = await supabaseClient.rpc("get_appointments_by_user_id", {
+    id: userId,
+  });
+
+  if (getAppointmentsResult.error) {
+    console.log(getAppointmentsResult.error);
+    throw new Error("Something went wrong!!");
+  }
+
+  return getAppointmentsResult.data.map((appointment) => ({
+    appointmentId: appointment.id,
+    title: appointment.title,
+    ownerId: appointment.owner_id,
+    ownerName: appointment.username,
+    ownerProfilePic: appointment.image,
+    location: appointment.location,
+    startDateTime: dayjs(appointment.start_time),
+    endDateTime: dayjs(appointment.end_time),
+    participantAmount: appointment.participant_number,
+  }));
 }
--- src/types/Appointment.tsx
+++ src/types/Appointment.tsx
@@ -1,10 +1,32 @@
+import { Dayjs } from "dayjs";
+
 export type Appointment = {
-    title: string,
-    ownerId: string;
-    ownerName: string;
-    ownerProfilePic: string;
-    location: string;
-    startDateTime: string;
-    endDateTime: string;
-    participantAmount: number;
-}
+  appointmentId: string;
+  title: string;
+  ownerId: string;
+  ownerName: string;
+  ownerProfilePic: string;
+  location: string;
+  startDateTime: Dayjs;
+  endDateTime: Dayjs;
+  participantAmount: number;
+};
+
+export type AppointmentDetail = {
+  detailHeader: AppointmentDetailHeader;
+  ownerId: string;
+  images: string[];
+  participantAmount: number;
+  pendingParticipants: string[];
+  acceptParticipants: string[];
+  rejectParticipants: string[];
+};
+
+export type AppointmentDetailHeader = {
+  title: string;
+  location: string;
+  startDateTime: Dayjs;
+  endDateTime: Dayjs;
+  tags: string[];
+  description: string;
+};
