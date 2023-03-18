fac116a5bf7f46af6656bcb3bbfa11a5a2e656fd
Chinnapat Nitipornsri <kharkkr@gmail.com>
2023-03-18 17:09:54 +0700

    Rename textfield component

--- src/components/public/CommonTextField.tsx
+++ src/components/public/CommonTextField.tsx
@@ -1,18 +1,26 @@
-import { Box, FormHelperText, TextField, Typography } from "@mui/material";
+import {
+  Box,
+  FormHelperText,
+  Icon,
+  InputAdornment,
+  Stack,
+  TextField,
+  Typography,
+} from "@mui/material";
+import EmailIcon from "@mui/icons-material/Email";
+import EditIcon from "@mui/icons-material/Edit";
+import { ICONS } from "enum/ICONS";
 
 type props = {
+  name?: string;
   header?: string;
-  label?: string;
-  placeholder?: string;
+  icon: string;
+  placeholder: string;
   value: string;
-  handleValueChange: (
-    event: React.ChangeEvent<HTMLTextAreaElement | HTMLInputElement>
-  ) => void;
-  isMultiLine?: boolean;
+  handleValueChange: (event: React.ChangeEvent<HTMLTextAreaElement | HTMLInputElement>) => void;
   char_limit?: number;
   isErr: boolean;
   errMsg: string;
-  mediumSize?: boolean;
 };
 
 const helperText = {
@@ -21,31 +29,60 @@ const helperText = {
   justifyContent: "space-between",
 };
 
-export default function CommonTextField(props: props) {
-  const exceedChar: boolean = props.char_limit
-    ? props.value.length > props.char_limit
-    : false;
+export default function NormalTextField(props: props) {
+  const exceedChar: boolean = props.char_limit ? props.value.length > props.char_limit : false;
+
+  function displayStartIcon(iconName: string) {
+    if (iconName === ICONS.MAIL) {
+      return <EmailIcon fontSize="large" color={props.isErr ? "error" : "secondary"} />;
+    } else if (iconName === ICONS.EDIT) {
+      return <EditIcon fontSize="large" color={props.isErr ? "error" : "secondary"} />;
+    }
+  }
 
   return (
     <>
-      <Typography variant="body1">{props?.header}</Typography>
-      <TextField
-        label={props?.label}
-        placeholder={props?.placeholder}
-        error={props.isErr || exceedChar}
-        value={props.value}
-        onChange={props.handleValueChange}
-        multiline={props.isMultiLine || false}
-        rows={props.isMultiLine ? 4 : 1}
-        fullWidth
-        size={props.mediumSize ? "medium" : "small"}
-      />
-      <Box sx={helperText}>
-        <FormHelperText error={exceedChar}>
-          {props.char_limit && `${props.value.length}/${props.char_limit}`}
-        </FormHelperText>
-        {props.isErr && <FormHelperText error>{props.errMsg}</FormHelperText>}
-      </Box>
+      <Stack spacing={1}>
+        <Box>
+          <Typography variant="body1">{props?.header}</Typography>
+          <TextField
+            name={props.name}
+            sx={{ backgroundColor: "#ffffff" }}
+            fullWidth
+            placeholder={props.placeholder}
+            value={props.value}
+            error={props.isErr}
+            onChange={props.handleValueChange}
+            inputProps={{
+              sx: {
+                textAlign: "center",
+                "&::placeholder": {
+                  textAlign: "center",
+                },
+              },
+            }}
+            InputProps={{
+              startAdornment: (
+                <InputAdornment position="start">{displayStartIcon(props.icon)}</InputAdornment>
+              ),
+              endAdornment: (
+                <InputAdornment position="start">
+                  <Icon fontSize="large" />
+                </InputAdornment>
+              ),
+            }}
+          />
+        </Box>
+        <Box sx={helperText}>
+          <FormHelperText error={exceedChar}>
+            {props.char_limit && `${props.value.length}/${props.char_limit}`}
+          </FormHelperText>
+          <FormHelperText error>
+            {props.isErr && props.errMsg}
+            {"\u00A0"}
+          </FormHelperText>
+        </Box>
+      </Stack>
     </>
   );
 }
