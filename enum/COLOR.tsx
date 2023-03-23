abe0a53989c8a2dc18a5af9a85a53449cf62a459
Chinnapat Nitipornsri <kharkkr@gmail.com>
2023-03-24 01:22:53 +0700

    Add color code enum

--- enum/COLOR.tsx
+++ enum/COLOR.tsx
@@ -3,3 +3,10 @@ export enum COLOR {
   NATURAL = "Natural",
   ERROR = "Error",
 }
+
+export enum COLOR_CODE {
+  ERROR = "#ff0000",
+  BLACK = "#000000",
+  DISABLE = "#E8E8E8",
+  WHITE = "#FFFFFF"
+}
