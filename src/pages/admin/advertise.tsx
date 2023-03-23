922aefc4c4c36c462984466a095ac2dc1a1b35f7
Nichapat Kerkkittikul <palm.nichapat@gmail.com>
2023-03-23 16:10:12 +0700

    Prepare code for server

--- src/pages/admin/advertise.tsx
+++ src/pages/admin/advertise.tsx
@@ -1,23 +1,35 @@
 import AdminNavbar from "@/components/admin/AdminNavbar";
 import CommonButton from "@/components/public/CommonButton";
 import Loading from "@/components/public/Loading";
-import { Typography } from "@mui/material";
+import { IconButton, Typography } from "@mui/material";
+import CameraAltIcon from "@mui/icons-material/CameraAlt";
 
 import { COLOR } from "enum/COLOR"
 import { PAGE_PATHS } from "enum/PAGES"
 
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { NextRouter, useRouter } from "next/router";
 import { userContext } from "supabase/user_context";
 
 export default function Advertise() {
     const router: NextRouter = useRouter();
     const userStatus = useContext(userContext);
+    const [fileImage, setFileImage] = useState<File | null>(null);
 
     function onSubmit(): void {
         //BACKEND should be implemented here!
-        console.log("Add button is clicked!")
+        // Use tempAdvertiseOwner,tempDuration, fileImage send to backend
+        const tempAdvertiseOwner: string = "Chula new course"
+        const tempDuration: number = 123
+        console.table([tempAdvertiseOwner, tempDuration])
+        console.log(fileImage)
     }
+
+    const handleImageChange = (event: any): void => {
+        const tempFile = event.target.files[0];
+        setFileImage(tempFile)
+        event.target.value = null;
+    };
 
     function backToAdminHome(): void {
         router.push(`${PAGE_PATHS.ADMIN_HOME}${userStatus.user?.userId}`)
@@ -38,6 +50,13 @@ export default function Advertise() {
         <>
             <AdminNavbar />
             <Typography variant="h1">Add Advertisement</Typography>
+            <IconButton
+                aria-label="upload picture"
+                component="label"
+            >
+                <input onChange={handleImageChange} hidden accept="image/*" type="file" />
+                <CameraAltIcon sx={{ fontSize: "50px" }} />
+            </IconButton>
             <CommonButton label={"Cancel"} color={COLOR.NATURAL} onClick={backToAdminHome} />
             <CommonButton label={"Add"} onClick={onSubmit} />
         </>
