ead11eb9f87dd03a5c6576e03d84a15b38981b14
Jinnapat <jaja_yana@hotmail.com>
2023-03-24 01:49:58 +0700

    Fix import host view appointment

--- src/pages/appointment/host/[appointment_id].tsx
+++ src/pages/appointment/host/[appointment_id].tsx
@@ -1,19 +1,13 @@
 import React, { useContext, useEffect, useState } from "react";
 import Navbar from "@/components/public/Navbar";
-import { Typography, Box, Link, Button, Grid, IconButton, Stack, Card, FormHelperText } from "@mui/material";
+import { Typography, Box, IconButton, Stack, Card, FormHelperText } from "@mui/material";
 import ArrowBackIcon from "@mui/icons-material/ArrowBack";
-import AppointmentParticipantCard from "@/components/appointment/AppointmentParticipantCard";
 import { AppointmentDetail } from "@/types/Appointment";
-import {
-  AcceptAppointment,
-  GetAppointmentsByAppointmentId,
-  RejectAppointment,
-} from "@/services/Appointments";
+import { GetAppointmentByAppointmentId } from "@/services/Appointment";
 import { useRouter } from "next/router";
 import { useSupabaseClient } from "@supabase/auth-helpers-react";
 import { Database } from "supabase/db_types";
 import Loading from "@/components/public/Loading";
-import ConfirmApptDialog from "@/components/appointment/ConfirmApptDialog";
 import { PAGE_PATHS, ROLE } from "enum/PAGES";
 import { userContext } from "supabase/user_context";
 import TitleTextField from "@/components/post/TitleTextField";
@@ -23,7 +17,6 @@ import Tags from "@/components/post/SelectTags";
 import { Tag } from "@/types/Tag";
 import DescriptionTextField from "@/components/public/DescriptionTextField";
 import DisplayImages from "@/components/post/DisplayImages";
-import Participant from "@/components/post/Participant";
 import { User } from "@/types/User";
 
 const HostAppointmentStyle = {
@@ -50,7 +43,7 @@ export default function Home() {
 
   const appointmentId = parseInt(router.query.appointment_id as string);
   useEffect(() => {
-    GetAppointmentsByAppointmentId(appointmentId, supabaseClient)
+    GetAppointmentByAppointmentId(appointmentId, supabaseClient)
       .then((appointment) => {
         setAppointment(appointment);
         setIsParticipant(appointment.ownerId !== userStatus.user?.userId);
