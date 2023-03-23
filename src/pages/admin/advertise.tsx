747bf1cdb2cbb68bc5da8f502fac0c22290c6bf8
Nichapat Kerkkittikul <palm.nichapat@gmail.com>
2023-03-23 15:36:12 +0700

    Implement action button

--- src/pages/admin/advertise.tsx
+++ src/pages/admin/advertise.tsx
@@ -1,7 +1,34 @@
 import AdminNavbar from "@/components/admin/AdminNavbar";
+import CommonButton from "@/components/public/CommonButton";
+import { Typography } from "@mui/material";
+
+import { COLOR } from "enum/COLOR"
+import { PAGE_PATHS } from "enum/PAGES"
+
+import { useContext } from "react";
+import { NextRouter, useRouter } from "next/router";
+import { userContext } from "supabase/user_context";
 
 export default function Advertise() {
+    const router: NextRouter = useRouter();
+    const userStatus = useContext(userContext);
+
+    function onSubmit(): void {
+        //BACKEND should be implemented here!
+        console.log("Add button is clicked!")
+    }
+
+    function backToAdminHome(): void {
+        router.push(`${PAGE_PATHS.ADMIN_HOME}${userStatus.user?.userId}`)
+        return
+    }
+
     return (
-        <AdminNavbar />
+        <>
+            <AdminNavbar />
+            <Typography variant="h1">Add Advertisement</Typography>
+            <CommonButton label={"Cancel"} color={COLOR.NATURAL} onClick={backToAdminHome} />
+            <CommonButton label={"Add"} onClick={onSubmit} />
+        </>
     )
 }
