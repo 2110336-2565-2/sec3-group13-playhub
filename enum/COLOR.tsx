9b70f09904ca6d1eb90c7075213514e1ffb56789
Chinnapat Nitipornsri <kharkkr@gmail.com>
2023-03-24 23:57:55 +0700

    Add accept and primary color code

--- enum/COLOR.tsx
+++ enum/COLOR.tsx
@@ -8,5 +8,7 @@ export enum COLOR_CODE {
   ERROR = "#ff0000",
   BLACK = "#000000",
   DISABLE = "#E8E8E8",
-  WHITE = "#FFFFFF"
+  WHITE = "#FFFFFF",
+  ACCEPT = "#42FF00",
+  PRIMARY = "#ffa31a"
 }
