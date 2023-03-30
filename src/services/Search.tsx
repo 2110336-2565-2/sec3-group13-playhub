9f6a35f281ca3d232b0d4c46604eaa5ae773a053
Jinnapat <jaja_yana@hotmail.com>
2023-03-30 11:39:02 +0700

    Add types to variables

--- src/services/Search.tsx
+++ src/services/Search.tsx
@@ -1,4 +1,5 @@
 import { Post } from "@/types/Post";
+import { Tag } from "@/types/Tag";
 import { SupabaseClient } from "@supabase/supabase-js";
 import { Database } from "supabase/db_types";
 import { dayjsWithoutTZ } from "./Posts";
@@ -26,12 +27,12 @@ export async function SearchPostByConditions(
     ownerName: post.owner_name,
     ownerProfilePic: post.owner_profile,
     location: post.location,
-    tags: post.tags.map((tag) => tag.name),
+    tags: post.tags.map((tag: Tag) => tag.name),
     description: post.description,
     image: post.images,
     startDateTime: dayjsWithoutTZ(post.end_time).format("DD/MM/YYYY hh:mm A"),
     endDateTime: dayjsWithoutTZ(post.end_time).format("DD/MM/YYYY hh:mm A"),
-    participants: post.participants.map((user) => ({
+    participants: post.participants.map((user: any) => ({
       userId: user.id,
       username: user.username,
       sex: user.sex,
