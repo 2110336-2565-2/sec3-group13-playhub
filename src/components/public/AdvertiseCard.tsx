4e05f0a5ea7c69c1abff70135e8291d46c30cbd1
Nichapat Kerkkittikul <palm.nichapat@gmail.com>
2023-03-30 23:22:11 +0700

    Border radius for advertise image

--- src/components/public/AdvertiseCard.tsx
+++ src/components/public/AdvertiseCard.tsx
@@ -1,5 +1,5 @@
 export default function AdvertiseCard({ ...props }) {
     return (
-        <img width={"100%"} src={props.src} style={{ marginTop: "50px" }} />
+        <img width={"100%"} src={props.src} style={{ marginTop: "50px", borderRadius: "16px" }} />
     )
 }
