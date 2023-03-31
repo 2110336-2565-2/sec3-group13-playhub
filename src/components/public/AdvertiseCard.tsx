6438e8e1789da4677911fc6d3378c2b96b762472
Nichapat Kerkkittikul <palm.nichapat@gmail.com>
2023-03-31 17:09:38 +0700

    Determine size of poster size

--- src/components/public/AdvertiseCard.tsx
+++ src/components/public/AdvertiseCard.tsx
@@ -1,5 +1,25 @@
-export default function AdvertiseCard({ ...props }) {
+import { Card, CardMedia } from "@mui/material";
+
+type props = {
+    src: string
+}
+
+export default function AdvertiseCard({ ...props }: props) {
     return (
-        <img width={"100%"} src={props.src} style={{ marginTop: "50px", borderRadius: "16px" }} />
+        <Card
+            sx={{
+                width: "970px",
+                height: "250px",
+                maxWidth: "100%",
+                boxShadow: "0px 0px 0px",
+                margin: "50px auto 0 auto"
+            }}>
+            <CardMedia
+                sx={{ height: "100%", width: "100%" }}
+                image={props.src}
+                title="poster file"
+            />
+        </Card>
     )
+
 }
