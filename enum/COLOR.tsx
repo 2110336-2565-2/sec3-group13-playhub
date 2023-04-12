fefe7af2fc84940cd674781be794778ade21cac7
Chinnapat Nitipornsri <kharkkr@gmail.com>
2023-04-13 00:07:35 +0700

    Add enums

---
+++ enum/COLOR.tsx
@@ -0,0 +1,14 @@
+export enum COLOR {
+  PRIMARY = "Primary",
+  NATURAL = "Natural",
+  ERROR = "Error",
+}
+
+export enum COLOR_CODE {
+  ERROR = "#ff0000",
+  BLACK = "#000000",
+  DISABLE = "#E8E8E8",
+  WHITE = "#FFFFFF",
+  ACCEPT = "#42FF00",
+  PRIMARY = "#ffa31a",
+}
---
+++ enum/INPUT_LIMIT.tsx
@@ -0,0 +1,30 @@
+export enum CHAR_LIMIT {
+  MIN_PASSWORD = 6,
+
+  MIN_DISPLAY_NAME = 1,
+  MAX_DISPLAY_NAME = 100,
+
+  MIN_DESCRIPTION = 1,
+  MAX_DESCRIPTION = 500,
+
+  MIN_TITLE = 1,
+  MAX_TITLE = 100,
+
+  MAX_NATIONAL_ID_CARD_NUMBER = 13,
+}
+
+export enum TAG_LIMIT {
+  MIN_TAG = 1,
+  MAX_TAG = 5,
+}
+
+export enum IMAGE_LIMIT {
+  MIN_IMAGE = 0,
+  MAX_IMAGE = 6,
+
+  // 1MB = 1_000_000 bytes
+  MAX_IMAGE_SIZE = 1_000_000,
+
+  // testing : 30KB = 30_000
+  // MAX_IMAGE_SIZE = 30_000,
+}
