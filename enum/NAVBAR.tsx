38d688a9e334f5bc63ccd432af848b50055d8fe1
Chinnapat Nitipornsri <kharkkr@gmail.com>
2023-04-12 23:31:01 +0700

    Change enum file name

--- enum/NAVBAR.tsx
+++ enum/NAVBAR.tsx
@@ -1,13 +1,6 @@
-export enum NAVBAR_PAGES {
-  HOME = "Home",
-  MY_POST = "My Post",
-  PROFILE = "Profile",
-  MY_APPOINTMENT = "My Appointment",
-  LOGOUT = "Logout",
-  VERIFY = "Verify",
-  POST = "Post",
-  ADVERTISEMENT = "Advertisement",
-  APPOINTMENT = "Appointment",
-  RATE = "Rate",
-  CONFIRM_REJECT = "Confirm/Reject",
-}
+export enum NavbarPages {
+    home = "Home",
+    myPost = "My Post",
+    profile = "Profile",
+    logout = "Logout",
+  }
--- enum/PAGES.tsx
+++ enum/PAGES.tsx
@@ -1,43 +1,16 @@
-export enum PAGE_PATHS {
+export enum PagePaths {
   // not require auth
-  LOGIN = "/login/",
-  REGISTER = "/register/",
+  login = "/login",
+  register = "/register",
 
   // require auth <-> exist userStatus
-  HOME = "/",
-
-  MY_POSTS = "/myPosts/",
-  POST = "/post/", // subfix : post_id
-  EDIT_POST = "/editPost/", // subfix : post_id
-  CREATE_POST = "/createPost/",
-
-  PROFILE = "/profile/", // subfix : user_id
-  EDIT_PROFILE = "/editProfile/",
-
-  VERIFY = "/verify/",
-
-  RESET_PASSWORD = "/resetPassword/",
-  SUCCESS_RESET_PASSWORD = "/successResetPassword/",
-
-  REQUEST_RESET_PASSWORD = "/requestResetPassword/",
-  SUCCESS_REQUEST_RESET_PASSWORD = "/successRequestResetPassword/",
-
-  APPOINTMENT = "/appointment/", //suffix : appointment_id
-  MY_APPOINTMENTS = "/myAppointments/",
-  SELECT_APPOINTMENT = "/selectAppointment/",
-  CONFIRM_APPOINTMENT = "/confirmAppointment/", //suffix : appointment_id
-  CREATE_APPOINTMENT = "/createAppointment/", // subfix : postId
-
-  SELECT_RATE = "/selectRate/",
-  RATE = "/rate/", //suffix : appointment_id
+  home = "/",
+  myPosts = "/myPosts",
+  editPost = "/editPost/", // subfix : postId
+  createPost = "/createPost",
+  profile = "/profile/", // subfix : username
+  editProfile = "/editProfile",
 
   // admin
-  ADMIN_HOME = "/admin/", // subfix : user_id
-  ADMIN_PROFILE = "/admin/profile/", // subfix : user_id
-  ADVERTISEMENT = "/admin/advertise"
-}
-
-export enum ROLE {
-  HOST = "host/",
-  PARTICIPANT = "participant/",
+  adminHome = "/admin/", // subfix : user_id
 }
