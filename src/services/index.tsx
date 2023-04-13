6f5c320d9fd268367430a7032136a2a04b09b7cc
Jinnapat <jaja_yana@hotmail.com>
2023-04-13 21:31:58 +0700

    Add Service class

---
+++ src/services/index.tsx
@@ -0,0 +1,42 @@
+import { SupabaseClient } from "@supabase/supabase-js";
+import { Database } from "supabase/db_types";
+import { UserService } from "./User";
+import { AdvertisementService } from "./Advertisement";
+import { AppointmentService } from "./Appointment";
+import { ParticipantService } from "./Participant";
+import { PasswordService } from "./Password";
+import { PostService } from "./Posts";
+import { ProfileService } from "./Profile";
+import { ReviewService } from "./Review";
+import { SearchService } from "./Search";
+import { TagService } from "./Tags";
+
+export class Service {
+  supabaseClient: SupabaseClient<Database>;
+  advertisement: AdvertisementService;
+  appointment: AppointmentService;
+  participant: ParticipantService;
+  password: PasswordService;
+  post: PostService;
+  profile: ProfileService;
+  review: ReviewService;
+  search: SearchService;
+  tag: TagService;
+  user: UserService;
+
+  constructor() {
+    const supabaseURL: string = process.env.NEXT_PUBLIC_SUPABASE_URL!;
+    const supabaseKey: string = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
+    this.supabaseClient = new SupabaseClient<Database>(supabaseURL, supabaseKey);
+    this.advertisement = new AdvertisementService(this.supabaseClient);
+    this.appointment = new AppointmentService(this.supabaseClient);
+    this.participant = new ParticipantService(this.supabaseClient);
+    this.password = new PasswordService(this.supabaseClient);
+    this.post = new PostService(this.supabaseClient);
+    this.profile = new ProfileService(this.supabaseClient);
+    this.review = new ReviewService(this.supabaseClient);
+    this.search = new SearchService(this.supabaseClient);
+    this.tag = new TagService(this.supabaseClient);
+    this.user = new UserService(this.supabaseClient);
+  }
+}
