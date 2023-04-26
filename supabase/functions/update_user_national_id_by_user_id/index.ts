f0b1adc3e86f90e5cd34e47a530acd6383b89215
Jinnapat <jaja_yana@hotmail.com>
2023-04-26 19:20:52 +0700

    update jest test case

--- supabase/functions/update_user_national_id_by_user_id/index.ts
+++ supabase/functions/update_user_national_id_by_user_id/index.ts
@@ -2,12 +2,40 @@
 // https://deno.land/manual/getting_started/setup_your_environment
 // This enables autocomplete, go to definition, etc.
 
+import * as postgres from "https://deno.land/x/postgres@v0.14.2/mod.ts";
 import { serve } from "https://deno.land/std@0.168.0/http/server.ts";
 import { updateNationalID } from "./updateNationalID.ts";
 
+const databaseUrl = Deno.env.get("SUPABASE_DB_URL")!;
+const pool = new postgres.Pool(databaseUrl, 3, true);
+
 serve(async (req: Request) => {
-  const body = await req.json();
-  return updateNationalID(body);
+  try {
+    const connection = await pool.connect();
+    try {
+      const body = await req.json();
+      const idSearchResult =
+        await connection.queryObject`SELECT id FROM public.user WHERE national_id=${body.national_id}`;
+      if (idSearchResult.rows.length > 0) {
+        return new Response(JSON.stringify({ message: "national_id is already exist" }), {
+          status: 403,
+        });
+      }
+      const result = updateNationalID(body);
+      if (result.success) {
+        await connection.queryObject`UPDATE public.user SET national_id=${body.national_id} 
+          WHERE id=${body.id}`;
+      }
+      return new Response(JSON.stringify({ message: result.message }), {
+        headers: { "Content-Type": "application/json" },
+        status: result.success ? 200 : 403,
+      });
+    } finally {
+      connection.release();
+    }
+  } catch (err) {
+    return new Response(String(err?.message ?? err), { status: 500 });
+  }
 });
 
 // To invoke:
--- supabase/functions/update_user_national_id_by_user_id/updateNationalID.ts
+++ supabase/functions/update_user_national_id_by_user_id/updateNationalID.ts
@@ -1,43 +1,39 @@
-type MessageResponse = {
-  message: string;
-  success: Boolean;
-};
-
-function createResponse(data: MessageResponse) {
-  return new Response(JSON.stringify(data), {
-    headers: { "Content-Type": "application/json" },
-  });
-}
-
 export function updateNationalID(body: any) {
   const national_id: any = body.national_id;
 
+  if (!body.id) {
+    return {
+      message: "id field is needed",
+      success: false,
+    };
+  }
+
   if (!national_id) {
-    return createResponse({
+    return {
       message: "national_id field is needed",
       success: false,
-    });
+    };
   }
 
   if (typeof national_id != "string") {
-    return createResponse({
+    return {
       message: "national_id needs to be a string",
       success: false,
-    });
+    };
   }
 
   if (national_id.length != 13) {
-    return createResponse({
+    return {
       message: "national_id length needs to be 13 characters",
       success: false,
-    });
+    };
   }
 
   if (!RegExp(/\d{13}/).test(national_id)) {
-    return createResponse({
+    return {
       message: "some characters are not an integer",
       success: false,
-    });
+    };
   }
 
   const national_string: string = national_id;
@@ -49,14 +45,14 @@ export function updateNationalID(body: any) {
   const thirdStep: number = (11 - secondStep) % 10;
 
   if (thirdStep !== parseInt(national_string[12])) {
-    return createResponse({
+    return {
       message: "last digit is not valid",
       success: false,
-    });
+    };
   }
 
-  return createResponse({
+  return {
     message: "ok",
     success: true,
-  });
+  };
 }
