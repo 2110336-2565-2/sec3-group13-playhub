70505da1e6d933006a4733b143b659126541010d
Theerachot <77727884+TheerachotTle@users.noreply.github.com>
2023-04-28 23:29:50 +0700

    fix upload image (#20)
    Co-authored-by: Jinnapat <jaja_yana@hotmail.com>

--- src/services/Advertisement.tsx
+++ src/services/Advertisement.tsx
@@ -13,16 +13,15 @@ export class AdvertisementService {
   async CreateAdvertisement(title: string, duration: number, image: File): Promise<void> {
     const currentDate: Dayjs = dayjs();
     const endDate: Dayjs = currentDate.add(duration, "day");
+    const imageName = `${currentDate.valueOf()}-${endDate.valueOf()}`;
     const uploadImageResult = await this.supabaseClient.storage
       .from("advertise")
-      .upload(currentDate.valueOf() + image.name, image);
+      .upload(imageName, image);
     if (uploadImageResult.error) {
       console.log(uploadImageResult.error);
       throw new Error("UploadImage Error!!!");
     }
-    const imageUrlResult = this.supabaseClient.storage
-      .from("advertise")
-      .getPublicUrl(currentDate.valueOf() + image.name);
+    const imageUrlResult = this.supabaseClient.storage.from("advertise").getPublicUrl(imageName);
     const addAdvertisementResult = await this.supabaseClient.rpc("create_advertisement", {
       title: title,
       end_date: endDate.toISOString(),
@@ -50,4 +49,20 @@ export class AdvertisementService {
       image_url: ad.image_url,
     }));
   }
+}
+
+export async function GetAdvertisementUrl(
+  supabaseClient: SupabaseClient<Database>
+): Promise<Advertise[]> {
+  const advertisementResult = await supabaseClient.rpc("get_advertisement");
+  if (advertisementResult.error) {
+    console.log(advertisementResult.error);
+    throw new Error("Get Advertisement Error!!");
+  }
+  if (!advertisementResult.data) {
+    throw new Error("No advertisement data found");
+  }
+  return advertisementResult.data.map((ad) => ({
+    image_url: ad.image_url,
+  }));
 }
